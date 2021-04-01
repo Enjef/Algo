a70e07d6205ae68db35ee4c82da39b496f7ebdd1
Evgeniy <ferno1@yandex.ru>
2021-04-01 14:20:43 +0300

    '7'

---
+++ 7 - Reverse Integer/7 - Reverse Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            x = -int(str(x)[:0:-1])
+        else:
+            x = int(str(x)[::-1])
+        if -2**31 <= x <= 2**31 - 1:
+            return x
+        return 0
+
+x = Solution()
+print(x.reverse(-12345))

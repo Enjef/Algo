81f5bc782285531ce01c8a8e421f33c4a36cd5db
Evgeniy <ferno1@yandex.ru>
2021-04-09 12:39:01 +0300

    20

---
+++ 20 - Valid Parentheses/20 - Valid Parentheses.py
@@ -0,0 +1,31 @@
+dict_a = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+}
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 1:
+            return False
+        if s[0] not in dict_a:
+            return False
+        temp = ''
+        for i in range(len(s)):
+            if s[i] in dict_a:
+                temp += s[i]
+                continue
+            if s[i] not in dict_a:
+                if not temp or s[i] != dict_a[temp[-1]]:
+                    return False
+                if temp and s[i] == dict_a[temp[-1]]:
+                    temp = temp[:-1]
+                    continue
+        if not temp:
+            return True
+        return False
+
+
+x = Solution()
+print(x.isValid(r'(('))

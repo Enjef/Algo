5a10bd19a17583bc5493f5642b03c587fc292be1
Evgeniy <ferno1@yandex.ru>
2021-04-11 15:00:19 +0300

    27 updated

--- 27 - Remove Element/27 - Remove Element.py
+++ 27 - Remove Element/27 - Remove Element.py
@@ -1,23 +1,13 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        if len(nums) == 1 and val == nums[0]:
-            return 0
-        count = 0
         i, j = 0, len(nums)-1
-        while i < j:
-            while nums[i] != val:
-                i += 1
-                if i > len(nums)-1:
-                    return len(nums)
-            while nums[j] == val:
-                count += 1
+        while i <= j:
+            if nums[i] == val:
+                nums[i], nums[j] = nums[j], nums[i]
                 j -= 1
-                if j < 0:
-                    return 0
-            if i >= j:
-                break
-            nums[i], nums[j] = nums[j], nums[i]
-        return len(nums) - count
+            else:
+                i += 1
+        return i
 
 
 x = Solution()

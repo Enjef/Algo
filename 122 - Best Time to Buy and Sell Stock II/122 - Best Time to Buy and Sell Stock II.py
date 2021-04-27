561f36060d791ef743553e12b5cf8c62ca40ce85
Evgeniy <ferno1@yandex.ru>
2021-04-27 14:44:59 +0300

    122

--- 122 - Best Time to Buy and Sell Stock II/122 - Best Time to Buy and Sell Stock II.py
+++ 122 - Best Time to Buy and Sell Stock II/122 - Best Time to Buy and Sell Stock II.py
@@ -17,6 +17,16 @@ class Solution:
 
         return profit
 
+    def maxProfit_fast(self, prices: List[int]) -> int:
+        profit = 0
+        prev = float('inf')
+
+        for cur in prices:
+            if cur > prev:
+                profit += cur - prev
+            prev = cur
+        return profit
+
 
 x = Solution()
 print(x.maxProfit([7,6,4,3,1]))

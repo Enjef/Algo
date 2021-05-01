4a0592186fb5372c49920c75a32732ae7a6c5fd6
Evgeniy <ferno1@yandex.ru>
2021-05-01 13:15:10 +0300

    101 recursive

---
+++ 101 - Symmetric Tree/101 - Symmetric Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root) -> bool:
+        if not root:
+            return True
+        else:
+            return self.compare(root.left, root.right)
+
+    def compare(self, left, right):
+        if left and right:
+            return (left.val == right.val and
+                    self.compare(left.left, right.right) and
+                    self.compare(left.rigth, right.left)
+                    )
+        else:
+            return left == right

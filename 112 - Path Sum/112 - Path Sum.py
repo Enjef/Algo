83e56e047353b1094a044425d5183df8b1680112
Evgeniy <ferno1@yandex.ru>
2021-05-04 16:44:01 +0300

    112

--- 112 - Path Sum/112 - Path Sum.py
+++ 112 - Path Sum/112 - Path Sum.py
@@ -1,21 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        summ = 0
-        return self.summ_get(root, summ, targetSum)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-    def summ_get(self, root, summ, targetSum):
-        if not root:
-            return False
-        summ += root.val
-        if not root.left and not root.right:
-            if summ == targetSum:
-                return True
-        return (self.summ_get(root.left, summ, targetSum) or
-                self.summ_get(root.right, summ, targetSum)
-                )
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        curr_a, curr_b = headA, headB
+        while curr_a != curr_b:
+            curr_a = curr_a.next if curr_a else headB
+            curr_b = curr_b.next if curr_b else headA
+        return curr_a
+
+    def getIntersection_set(self, headA: ListNode, headB: ListNode) -> ListNode:
+        set_a = set()
+        while headA:
+            set_a.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in set_a:
+                return headB
+            headB = headB.next
+        return None

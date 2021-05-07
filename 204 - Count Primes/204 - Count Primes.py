51f1c335fa5a67cbdec2f3354254991bdcc2e959
Evgeniy <ferno1@yandex.ru>
2021-05-08 00:12:38 +0300

    204

--- 204 - Count Primes/204 - Count Primes.py
+++ 204 - Count Primes/204 - Count Primes.py
@@ -1,11 +1,12 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        n_list = list(range(2, n+1))
+        if n < 3:
+            return 0
+        n_list = list(range(2, n))
         for i in range(len(n_list)):
             if n_list[i] != 0:
                 for j in range(i+n_list[i], len(n_list), n_list[i]):
                     n_list[j] = 0
-            print(n_list)
         return len([x for x in n_list if x != 0])
 
 

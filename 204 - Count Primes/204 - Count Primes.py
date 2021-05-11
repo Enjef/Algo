d03f6b6f5c5caede0bd490371ba07fba249d99da
Evgeniy <ferno1@yandex.ru>
2021-05-11 11:18:20 +0300

    204

--- 204 - Count Primes/204 - Count Primes.py
+++ 204 - Count Primes/204 - Count Primes.py
@@ -3,10 +3,9 @@ class Solution:
         if n < 3:
             return 0
         n_list = list(range(2, n))
-        for i in range(len(n_list)):
-            if n_list[i] != 0:
-                for j in range(i+n_list[i], len(n_list), n_list[i]):
-                    n_list[j] = 0
+        for i in range(int(len(n_list)**0.5)):
+            if n_list[i]:
+                n_list[n_list[i]*n_list[i]-2: len(n_list): n_list[i]] = [0] * len(range(n_list[i]*n_list[i]-2, len(n_list), n_list[i]))
         return len([x for x in n_list if x != 0])
 
 

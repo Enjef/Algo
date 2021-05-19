1af8852acfe6a207f60c9f7bffe6a212050456ac
Evgeniy <ferno1@yandex.ru>
2021-05-20 01:31:31 +0300

    409

---
+++ 409 - Longest Palindrome/409 - Longest Palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        s_map = {}
+        for i in s:
+            if i not in s_map:
+                s_map[i] = 1
+            else:
+                s_map[i] += 1
+        out = 0
+        odd = False
+        for key in s_map:
+            if s_map[key] % 2 != 0:
+                out += s_map[key] - 1
+                odd = True
+            else:
+                out += s_map[key]
+        if odd:
+            out += 1
+        return out

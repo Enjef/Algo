0a0ea55878f794cd5989282d915fc41f09db6e8c
Evgeniy <ferno1@yandex.ru>
2021-05-20 02:24:51 +0300

    404

---
+++ 404 - Sum of Left Leaves/404 - Sum of Left Leaves.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        stack = [(root, False)]
+        out_sum = 0
+        while stack:
+            root, left = stack.pop()
+            if not root.left and not root.right and left:
+                out_sum += root.val
+            if root.left:
+                stack.append((root.left, True))
+            if root.right:
+                stack.append((root.right, False))
+        return out_sum

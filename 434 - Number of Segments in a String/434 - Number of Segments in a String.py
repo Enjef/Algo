4199493f52e9d271cf63a24dfb40bed8464d2489
Evgeniy <ferno1@yandex.ru>
2021-05-20 02:39:03 +0300

    434

--- 434 - Number of Segments in a String/434 - Number of Segments in a String.py
+++ 434 - Number of Segments in a String/434 - Number of Segments in a String.py
@@ -2,4 +2,4 @@ class Solution:
     def countSegments(self, s: str) -> int:
         if not s:
             return 0
-        return len(s.split())
+        return len(s.split()) # one string is enough

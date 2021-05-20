78a4477c1d76b215d9822b7d84f7c4a292363191
Evgeniy <ferno1@yandex.ru>
2021-05-21 02:31:27 +0300

    509

--- 509 - Fibonacci Number/509 - Fibonacci Number.py
+++ 509 - Fibonacci Number/509 - Fibonacci Number.py
@@ -9,3 +9,11 @@ class Solution:
             a, b = b, a + b
             n -= 1
         return b
+
+    def fib_for(self, n: int) -> int:
+        a, b = 0, 1
+        if n == 0:
+            return a
+        for i in range(n):
+            a, b = b, a + b
+        return a

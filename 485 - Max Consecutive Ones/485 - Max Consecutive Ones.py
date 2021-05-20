3b409b24ae81eb3eeaaea8a309e57886f6cb2c93
Evgeniy <ferno1@yandex.ru>
2021-05-21 01:07:38 +0300

    485

--- 485 - Max Consecutive Ones/485 - Max Consecutive Ones.py
+++ 485 - Max Consecutive Ones/485 - Max Consecutive Ones.py
@@ -4,9 +4,11 @@ class Solution:
         count = 0
         for i in nums:
             if i == 0:
+                if count > max_count:
+                    max_count = count
                 count = 0
             if i == 1:
                 count += 1
-                if count > max_count:
-                    max_count = count
+        if count > max_count:
+            max_count = count
         return max_count

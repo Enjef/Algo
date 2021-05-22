e9e06992ab4c4f86c8790a89cf0b1c742ffb6c38
Evgeniy <ferno1@yandex.ru>
2021-05-22 15:50:10 +0300

    541

--- 541 - Reverse String II/541 - Reverse String II.py
+++ 541 - Reverse String II/541 - Reverse String II.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverseStr(self, s: str, k: int) -> str:
+    def reverseStr_join_reversed(self, s: str, k: int) -> str:
         if len(s) < k:
             s = s[::-1]
             return s
@@ -8,4 +8,15 @@ class Solution:
             return s
         for i in range(0, len(s), 2*k):
             s = s[:i] + ''.join(reversed(s[i:i+k])) + s[i+k:]
+        return s
+
+    def reverseStr(self, s: str, k: int) -> str:
+        if len(s) < k:
+            s = s[::-1]
+            return s
+        if len(s) < 2*k:
+            s = s[k-1::-1] + s[k:]
+            return s
+        for i in range(0, len(s), 2*k):
+            s = s[:i] + s[i:i+k][::-1] + s[i+k:]
         return s

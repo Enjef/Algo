380a62c3ee3e688460d1eac63a85825a65d5629c
Evgeniy <ferno1@yandex.ru>
2021-05-23 17:30:32 +0300

    561

--- 561 - Array Partition I/561 - Array Partition I.py
+++ 561 - Array Partition I/561 - Array Partition I.py
@@ -1,8 +1,9 @@
+#  slow 9.3%
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
         summ = 0
-        for i in range(0,len(nums),2):
+        for i in range(0, len(nums), 2):
             summ += min(nums[i], nums[i+1])
         return summ
 

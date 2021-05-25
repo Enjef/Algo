b1715115df51d87a4463506d005b942337873ee8
Evgeniy <ferno1@yandex.ru>
2021-05-25 10:41:35 +0300

    605

--- 605 - Can Place Flowers/605 - Can Place Flowers.py
+++ 605 - Can Place Flowers/605 - Can Place Flowers.py
@@ -5,25 +5,25 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if len(flowerbed) == 1 and flowerbed[0]==0:
-            if n ==1:
+        if len(flowerbed) == 1 and flowerbed[0] == 0:
+            if n == 1:
                 return True
-        if n==0:
+        if n == 0:
             return True
-        if len(flowerbed)>1 and flowerbed[0]==flowerbed[1]==0:
-                flowerbed[0] = 1
-                n -= 1
-                if n <=0:
-                    return True
-            
-        for i in range(1,len(flowerbed)-1):
-            if flowerbed[i-1]==flowerbed[i]== flowerbed[i+1]==0:
+        if len(flowerbed) > 1 and flowerbed[0] == flowerbed[1] == 0:
+            flowerbed[0] = 1
+            n -= 1
+            if n <= 0:
+                return True
+
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1] == flowerbed[i] == flowerbed[i+1] == 0:
                 flowerbed[i] = 1
                 n -= 1
-                if n <=0:
+                if n <= 0:
                     return True
-        if len(flowerbed)>1 and flowerbed[-1]==flowerbed[-2]==0:
-                n -= 1
-                if n <=0:
-                    return True
+        if len(flowerbed) > 1 and flowerbed[-1] == flowerbed[-2] == 0:
+            n -= 1
+            if n <= 0:
+                return True
         return False

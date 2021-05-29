2a5157039aa54bfa3d930327d5057113664fb825
Evgeniy <ferno1@yandex.ru>
2021-05-29 11:20:45 +0300

    566

--- 566 - Reshape the Matrix/566 - Reshape the Matrix.py
+++ 566 - Reshape the Matrix/566 - Reshape the Matrix.py
@@ -2,7 +2,6 @@ class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         out = []
         row = []
-        k = 0
         if len(mat) * len(mat[0]) != c * r:
             return mat
         for item in mat:

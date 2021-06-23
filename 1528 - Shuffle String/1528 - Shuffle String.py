bc022974d06839f941bfec319f8d81782b49d7fa
Evgeniy <ferno1@yandex.ru>
2021-06-23 12:44:45 +0300

    1528

---
+++ 1528 - Shuffle String/1528 - Shuffle String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:  # 30%
+        res = [''] * len(s)
+        for i in range(len(res)):
+            res[i] = s[indices.index(i)]
+        return ''.join(res)
+
+    def restoreString(self, s: str, indices: List[int]) -> str:  # best
+        result = [""] * len(s)
+        for i, letter in enumerate(s):
+            result[indices[i]] = letter
+        return "".join(result) 

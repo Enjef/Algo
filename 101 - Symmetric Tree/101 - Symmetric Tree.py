a9569b4cc9bfdc291dd7a85d64d9ff237a8639e2
Evgeniy <ferno1@yandex.ru>
2021-07-27 17:19:39 +0300

    101

--- 101 - Symmetric Tree/101 - Symmetric Tree.py
+++ 101 - Symmetric Tree/101 - Symmetric Tree.py
@@ -5,15 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSymmetric(self, root) -> bool:
+    def isSymmetric(self, root: TreeNode) -> bool:
         if not root:
             return True
         return self.compare(root.left, root.right)
 
     def compare(self, left, right):
         if left and right:
-            return (left.val == right.val and
-                    self.compare(left.left, right.right) and
-                    self.compare(left.rigth, right.left)
-                    )
-        return left == right
+            return (
+                left.val == right.val and
+                self.compare(left.left, right.right) and
+                self.compare(left.right, right.left)
+            )
+        return left == right    

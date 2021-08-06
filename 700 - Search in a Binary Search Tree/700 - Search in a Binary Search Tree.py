2923b54c3404874bed8ea4292fde199d7bba8f82
Evgeniy <ferno1@yandex.ru>
2021-08-06 18:50:30 +0300

    700

--- 700 - Search in a Binary Search Tree/700 - Search in a Binary Search Tree.py
+++ 700 - Search in a Binary Search Tree/700 - Search in a Binary Search Tree.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:  # 50.72% 71.21%
         if not root:
             return root
         if root.val == val:
@@ -13,3 +13,16 @@ class Solution:
         return (
             self.searchBST(root.left, val) or self.searchBST(root.right, val)
         )
+
+    def helper(self, node, val):
+        if node is None:
+            return None
+        elif node.val == val:
+            return node
+        elif node.val > val:
+            return self.helper(node.left, val)
+        else:
+            return self.helper(node.right, val)
+
+    def searchBST_best(self, root: TreeNode, val: int) -> TreeNode:
+        return self.helper(root, val)

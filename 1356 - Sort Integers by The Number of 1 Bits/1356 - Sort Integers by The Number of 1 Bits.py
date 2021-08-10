6d5eb2fca99cb7bae2ae9a7534f655bea0eec5ce
Evgeniy <ferno1@yandex.ru>
2021-08-10 11:50:13 +0300

    1356

--- 1356 - Sort Integers by The Number of 1 Bits/1356 - Sort Integers by The Number of 1 Bits.py
+++ 1356 - Sort Integers by The Number of 1 Bits/1356 - Sort Integers by The Number of 1 Bits.py
@@ -1,10 +1,10 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:  # 94.21% 72.86%
         return [x[1] for x in sorted([(bin(x).count('1'), x) for x in arr])]
-        
+
     def sortByBits_best(self, arr: List[int]) -> List[int]:
         arr.sort()
-        arr.sort(key = lambda x:bin(x).count('1'))
+        arr.sort(key=lambda x:bin(x).count('1'))
         return arr
 
     def sortByBits_second_to_best(self, arr: List[int]) -> List[int]:

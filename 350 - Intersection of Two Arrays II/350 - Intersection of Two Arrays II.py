afc3baca9f9355323ac6eafe8795aee1ebe5f4dc
Evgeniy <ferno1@yandex.ru>
2021-08-30 20:31:48 +0300

    study plan day 3 intersection of two arrays II

--- 350 - Intersection of Two Arrays II/350 - Intersection of Two Arrays II.py
+++ 350 - Intersection of Two Arrays II/350 - Intersection of Two Arrays II.py
@@ -54,3 +54,22 @@ class Solution:
             else:
                 j += 1
         return out
+
+class Solution:
+    def intersect_study_plan_variant(
+            self,
+            nums1: List[int],
+            nums2: List[int]) -> List[int]: # 8.17% 90.08%
+        nums1_map = {}
+        nums2_map = {}
+        for num in nums1:
+            nums1_map[num] = nums1_map.get(num, 0) + 1
+        for num in nums2:
+            nums2_map[num] = nums2_map.get(num, 0) + 1
+        for num in nums1_map:
+            nums1_map[num] = min(nums1_map.get(num, 0), nums2_map.get(num, 0))
+        out = []
+        for el in nums1_map:
+            if nums1_map[el]:
+                out.extend([el] * nums1_map[el])
+        return out

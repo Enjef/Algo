b34a9ff491d94fb61191a6865faf242077cd87b0
Evgeniy <ferno1@yandex.ru>
2021-09-02 21:08:47 +0300

    387

--- 387 - First Unique Character in a String/387 - First Unique Character in a String.py
+++ 387 - First Unique Character in a String/387 - First Unique Character in a String.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def firstUniqChar(self, s):
+    def firstUniqChar(self, s):  #  60.72% 31.62%
         """
         :type s: str
         :rtype: int
@@ -13,3 +13,25 @@ class Solution(object):
         return(min(list(c_map.values())) if
                min(list(c_map.values())) != float('inf') else -1
                )
+
+    def firstUniqChar_sp_day_6(self, s: str) -> int:  # 12.73% 70.16%
+        x_dict = {}
+        for char in s:
+            x_dict[char] = x_dict.get(char, 0) + 1
+        for i, char in enumerate(s):
+            if x_dict[char] == 1:
+                return i
+        return -1
+
+    def firstUniqChar_best_speed(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = len(s)
+        for item in "abcdefghijklmnopqrstuvwxyz":
+            a = s.find(item)
+            b = s.rfind(item)
+            if a == b and a != -1:
+                l = min(l, a)
+        return l if l < len(s) else -1

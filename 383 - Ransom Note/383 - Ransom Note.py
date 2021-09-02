f755113b5c5b87e9442407cb058c4e99cd4ceb9a
Evgeniy <ferno1@yandex.ru>
2021-09-02 21:33:00 +0300

    383

--- 383 - Ransom Note/383 - Ransom Note.py
+++ 383 - Ransom Note/383 - Ransom Note.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canConstruct(self, ransomNote, magazine):
+    def canConstruct(self, ransomNote, magazine):  # 23.31% 11.67%
         """
         :type ransomNote: str
         :type magazine: str
@@ -11,5 +11,25 @@ class Solution:
             if ransom_note[i] in magazine:
                 magazine.remove(ransom_note[i])
             else:
+                return False
+        return True
+
+    def canConstruct_sp_day_6(
+            self,
+            ransomNote: str,
+            magazine: str) -> bool:  # 22.98% 11.67%
+        note = list(ransomNote)
+        mag = list(magazine)
+        while note:
+            cur = note.pop()
+            if cur in mag:
+                mag.remove(cur)
+            else:
+                return False
+        return True
+
+    def canConstruct_best_speed(self, ransomNote, magazine):
+        for i in set(ransomNote):
+            if ransomNote.count(i) > magazine.count(i):
                 return False
         return True

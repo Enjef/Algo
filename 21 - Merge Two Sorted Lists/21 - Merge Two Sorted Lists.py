110f15f5e5e6a3654e3ca00f181ec3bc31d0e438
Evgeniy <ferno1@yandex.ru>
2021-09-03 18:56:46 +0300

    21

--- 21 - Merge Two Sorted Lists/21 - Merge Two Sorted Lists.py
+++ 21 - Merge Two Sorted Lists/21 - Merge Two Sorted Lists.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1, l2):
+    def mergeTwoLists(self, l1, l2):  # 92.27%  61.84%
         l3 = ListNode()
         out = l3
         while l1 and l2:
@@ -17,3 +17,20 @@ class Solution:
             l3 = l3.next
         l3.next = l1 or l2
         return out.next
+
+    def mergeTwoLists_sp_day_7(self, l1, l2):  # 92.27% 31.47%
+        dummy = ListNode()
+        cur = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1:
+            cur.next = l1
+        elif l2:
+            cur.next = l2
+        return dummy.next

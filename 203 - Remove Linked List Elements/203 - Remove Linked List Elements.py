853cac52ea805cedc41af5a058bd3147ff5ab4cb
Evgeniy <ferno1@yandex.ru>
2021-09-03 20:39:46 +0300

    203

--- 203 - Remove Linked List Elements/203 - Remove Linked List Elements.py
+++ 203 - Remove Linked List Elements/203 - Remove Linked List Elements.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def removeElements(self, head, val):
+    def removeElements(self, head, val):  # 72.88 % 26.95%
         """
         :type head: ListNode
         :type val: int
@@ -20,5 +20,27 @@ class Solution(object):
                 prev.next = cur.next
             else:
                 prev = cur
+            cur = cur.next
+        return head
+
+    def removeElements_sp_day_7(self, head, val):  # 51.03% 26.95%
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        while head.val == val:
+            if not head.next:
+                return None
+            head = head.next
+        prev = ListNode(next=head)
+        cur = head
+        while cur:
+            if cur.val == val:
+                prev.next = cur.next
+            else:
+                prev = prev.next
             cur = cur.next
         return head

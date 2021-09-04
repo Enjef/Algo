8ca31dccd8df1f53c175cf4d07b11ebe5d79a00c
Evgeniy <ferno1@yandex.ru>
2021-09-04 21:18:38 +0300

    617

--- 617 - Merge Two Binary Trees/617 - Merge Two Binary Trees.py
+++ 617 - Merge Two Binary Trees/617 - Merge Two Binary Trees.py
@@ -37,12 +37,32 @@ class Solution:
             root2: TreeNode) -> TreeNode:
         if root1 is None:
             return root2
-
         if root2 is None:
             return root1
-
         root1.val += root2.val
         root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
 
+        return root1
+
+    def mergeTrees_sp_day_8(
+            self,
+            root1: Optional[TreeNode],
+            root2: Optional[TreeNode]) -> Optional[TreeNode]:  # 78.59% 52.17%
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        stack = [[root1, root2]]
+        while stack:
+            cur_one, cur_two = stack.pop()
+            cur_one.val += cur_two.val
+            if cur_one.left and cur_two.left:
+                stack.append([cur_one.left, cur_two.left])
+            if cur_one.right and cur_two.right:
+                stack.append([cur_one.right, cur_two.right])
+            if not cur_one.left and cur_two.left:
+                cur_one.left = cur_two.left
+            if not cur_one.right and cur_two.right:
+                cur_one.right = cur_two.right
         return root1

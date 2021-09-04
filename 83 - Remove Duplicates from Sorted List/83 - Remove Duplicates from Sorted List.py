513c4f0385fb61e86e3c18dc356ffd2a1a3ba446
Evgeniy <ferno1@yandex.ru>
2021-09-04 19:48:34 +0300

    83

--- 83 - Remove Duplicates from Sorted List/83 - Remove Duplicates from Sorted List.py
+++ 83 - Remove Duplicates from Sorted List/83 - Remove Duplicates from Sorted List.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:  # 98.83% 24.07%
         if not head or not head.next:
             return head
         prev = head
@@ -16,4 +16,18 @@ class Solution:
             else:
                 prev = curr
                 curr = curr.next
+        return head
+
+    def deleteDuplicates_sp_day_8(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:  # 5.51% 55.88%
+        if not head:
+            return head
+        cur = head
+        while cur:
+            if cur.next:
+                if cur.next.val == cur.val:
+                    cur.next = cur.next.next
+                    continue
+            cur = cur.next
         return head

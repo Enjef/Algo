340145e81c0ac90070d9dd6c943ce8e952ae74e4
Evgeniy <ferno1@yandex.ru>
2021-09-05 14:28:51 +0300

    232

--- 232 - Implement Queue using Stacks/232 - Implement Queue using Stacks.py
+++ 232 - Implement Queue using Stacks/232 - Implement Queue using Stacks.py
@@ -1,6 +1,6 @@
 class MyQueue:
 
-    def __init__(self):
+    def __init__(self):  # 81.23% 73.84%
         """
         Initialize your data structure here.
         """
@@ -37,9 +37,37 @@ class MyQueue:
         return not self.stack
 
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+class MyQueue_study_plan_day_9:  # 94.30% 44.31%
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+        self.size = 0
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+        self.size += 1
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.size -= 1
+        return self.queue.pop(0)
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return self.size == 0

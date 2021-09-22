6bbe3b20657871369eadcace1536f7a6ba298f2b
Evgeniy <ferno1@yandex.ru>
2021-09-22 11:47:04 +0300

    409

--- 409 - Longest Palindrome/409 - Longest Palindrome.py
+++ 409 - Longest Palindrome/409 - Longest Palindrome.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
+    def longestPalindrome(self, s: str) -> int:  # 92.46% 93.70%
         s_map = {}
         for i in s:
             if i not in s_map:
@@ -18,7 +18,7 @@ class Solution:
             out += 1
         return out
 
-    def longestPalindrome_set(self, s: str) -> int:
+    def longestPalindrome_set(self, s: str) -> int:  # 92.46% 93.70%
         s_set = set()
         for i in s:
             if i not in s_set:
@@ -26,3 +26,19 @@ class Solution:
             else:
                 s_set.remove(i)
         return len(s) if not s_set else len(s) - len(s_set) + 1
+
+    def longestPalindrome_study_plan_ds_day_6(
+            self,
+            s: str) -> int:  # 49.29% 54.72%
+        c_dict = {}
+        out = 0
+        spare_one = False
+        for char in s:
+            c_dict[char] = c_dict.get(char, 0) + 1
+        for item in c_dict:
+            if c_dict[item] % 2 == 0:
+                out += c_dict[item]
+            else:
+                out += c_dict[item] - 1
+                spare_one = True
+        return out + spare_one

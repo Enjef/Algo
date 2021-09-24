2bc94a977b89cc3a5c1454f3a2e7870704ce8c32
Evgeniy <ferno1@yandex.ru>
2021-09-24 22:19:47 +0300

    74

--- 74 - Search a 2D Matrix/74 - Search a 2D Matrix.py
+++ 74 - Search a 2D Matrix/74 - Search a 2D Matrix.py
@@ -20,20 +20,19 @@ class Solution:
                 left = mid + 1
         return False
 
-    def searchMatrix_refactored(
+    def searchMatrix_study_plan(
             self,
             matrix: List[List[int]],
-            target: int) -> bool:  # 64.28% 33.67%
-        left = 0
-        right = len(matrix) * len(matrix[0]) - 1
+            target: int) -> bool:  # 88.93% 31.37%
+        left, right = 0, len(matrix) * len(matrix[0]) - 1
         while left <= right:
             mid = left + (right - left) // 2
-            row_mid, col_mid = divmod(mid, len(matrix[0]))
-            if matrix[row_mid][col_mid] == target:
+            row, col = divmod(mid, len(matrix[0]))
+            if matrix[row][col] == target:
                 return True
-            if matrix[row_mid][col_mid] > target:
+            elif matrix[row][col] > target:
                 right = mid - 1
-            if matrix[row_mid][col_mid] < target:
+            elif matrix[row][col] < target:
                 left = mid + 1
         return False
 

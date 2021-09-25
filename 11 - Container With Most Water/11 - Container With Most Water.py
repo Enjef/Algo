9a58e65e24f9151f532ddfe86c8e6d0ffcb2757a
Evgeniy <ferno1@yandex.ru>
2021-09-25 19:32:49 +0300

    11

--- 11 - Container With Most Water/11 - Container With Most Water.py
+++ 11 - Container With Most Water/11 - Container With Most Water.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxArea(self, height) -> int:
+    def maxArea(self, height) -> int:  # 56.78% 94.71%
         v_max = float('-inf')
         left = 0
         right = len(height) - 1
@@ -13,6 +13,26 @@ class Solution:
                 right -= 1
         return v_max
 
+    def maxArea_study_plan(self, height: List[int]) -> int:  # 30.19% 57.02%
+        i, j = 0, len(height) - 1
+        v_max = 0
+        while i < j:
+            v_max = max(v_max, (j - i) * min(height[i], height[j]))
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return v_max
 
-x = Solution()
-print(x.maxArea([1, 2, 44, 4]))
+    def maxArea_best_speed(self, height: List[int]) -> int:
+        res = 0
+        r = len(height) - 1
+        l = 0
+        hmax = max(height)
+        while (r - l) * hmax >= res:
+            res = max(res, min(height[l], height[r])*(r-l))
+            if height[r] > height[l]:
+                l += 1
+            else:
+                r -= 1
+        return res

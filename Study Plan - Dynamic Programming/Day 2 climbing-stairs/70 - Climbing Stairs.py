10ba00aaa43f22fc309934b2a4ab86cbe819a45b
Evgeniy <ferno1@yandex.ru>
2021-10-06 21:36:41 +0300

    dp 70

--- Study Plan - Dynamic Programming/Day 2 climbing-stairs/70 - Climbing Stairs.py
+++ Study Plan - Dynamic Programming/Day 2 climbing-stairs/70 - Climbing Stairs.py
@@ -1,5 +1,5 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def climbStairs(self, n: int) -> int:  # 60.44% 71.70%
         fib = [1, 2]
         if n == 1:
             return fib[0]
@@ -9,3 +9,10 @@ class Solution:
             fib[0], fib[1] = fib[1], fib[0] + fib[1]
             n -= 1
         return fib[1]
+
+    def climbStairs_dp(self, n: int) -> int:  # 83.26% 90.44%
+        dp = [1, 1]
+        while n:
+            n -= 1
+            dp[0], dp[1] = dp[1], dp[0] + dp[1]
+        return dp[0]

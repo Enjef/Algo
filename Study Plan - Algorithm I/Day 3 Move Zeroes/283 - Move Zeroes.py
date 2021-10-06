e36963272a7483fd77a744dc46999be86d4aff4e
Evgeniy <ferno1@yandex.ru>
2021-10-06 23:42:34 +0300

    algo day 3

--- Study Plan - Algorithm I/Day 3 Move Zeroes/283 - Move Zeroes.py
+++ Study Plan - Algorithm I/Day 3 Move Zeroes/283 - Move Zeroes.py
@@ -1,5 +1,7 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes_count_zeroes(
+            self,
+            nums: List[int]) -> None:  # 87.50 % 88.31%
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -10,4 +12,21 @@ class Solution:
                 count += 1
         for i in range(count, len(nums)):
             nums[i] = 0
+        return
+
+    def moveZeroes_(self, nums: List[int]) -> None:  # 20.65% 23.19%
+        i = j = 0
+        while i < len(nums) and j < len(nums):
+            if i < j and nums[i] == 0 and nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+                continue
+            elif nums[i] != 0 and i < j:
+                i += 1
+            elif nums[j] == 0:
+                j += 1
+            else:
+                i += 1
+                j += 1
         return

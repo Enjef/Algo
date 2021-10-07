1b7771436f1391cac5f8b7c59288109b8d21f313
Evgeniy <ferno1@yandex.ru>
2021-10-07 12:01:37 +0300

    557

--- Study Plan - Algorithm I/Day 4 Reverse String/557 - Reverse Words in a String III.py
+++ Study Plan - Algorithm I/Day 4 Reverse String/557 - Reverse Words in a String III.py
@@ -1,9 +1,9 @@
 class Solution:
-    def reverseWords(self, s: str) -> str:
+    def reverseWords(self, s: str) -> str:  #  35.54% 61.37%
         temp = s.split()
         for i, word in enumerate(temp):
             temp[i] = word[::-1]
         return ' '.join(temp)
 
-    def reverseWords_list_comp(self, s: str) -> str:
+    def reverseWords_list_comp(self, s: str) -> str:  # 95.24% 74.97%
         return ' '.join([word[::-1] for word in s.split()])

c642b20be45943095e736f5467584074a193cb45
Evgeniy <ferno1@yandex.ru>
2021-10-07 22:45:34 +0300

    dp jump game

--- Study Plan - Dynamic Programming/Day 4 jump-game/45 - Jump Game II.py
+++ Study Plan - Dynamic Programming/Day 4 jump-game/45 - Jump Game II.py
@@ -10,3 +10,19 @@ class Solution:
                 cur_jump = max_jump
                 count += 1
         return count
+
+    def jump_dp(self, nums: List[int]) -> int:  # 95.81% 97.51%
+        if len(nums) == 1:
+            return 0
+        count = 0
+        cur = 0
+        dp = [0] * len(nums)
+        for i in range(len(nums)-1):
+            if dp[i-1]-1 < nums[i]:
+                dp[i] = nums[i]
+            else:
+                dp[i] = dp[i-1]-1
+            if i == cur:
+                count += 1
+                cur = dp[i]+i
+        return count
--- Study Plan - Dynamic Programming/Day 4 jump-game/55 - Jump Game.py
+++ Study Plan - Dynamic Programming/Day 4 jump-game/55 - Jump Game.py
@@ -6,3 +6,13 @@ class Solution:
             if i - jump_max >= 0:
                 return False
         return True
+        
+    def canJump_dp(self, nums: List[int]) -> bool:  # 57.41% 35.65%
+        if len(nums) == 1:
+            return True
+        dp = [0]*len(nums)
+        for i in range(len(nums)-1):
+            dp[i] = max(dp[i-1]-1, nums[i])
+            if dp[i] <= 0:
+                return False
+        return True

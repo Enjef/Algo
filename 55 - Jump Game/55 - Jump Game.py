99c5ef8a4a96cf5c852dab0210923325c6daa295
Evgeniy <ferno1@yandex.ru>
2021-10-07 22:44:33 +0300

    55

--- 55 - Jump Game/55 - Jump Game.py
+++ 55 - Jump Game/55 - Jump Game.py
@@ -13,3 +13,13 @@ class Solution:
             if i + nums[i] >= last:
                 last = i
         return last == 0
+
+    def canJump_dp(self, nums: List[int]) -> bool:  # 57.41% 35.65%
+        if len(nums) == 1:
+            return True
+        dp = [0]*len(nums)
+        for i in range(len(nums)-1):
+            dp[i] = max(dp[i-1]-1, nums[i])
+            if dp[i] <= 0:
+                return False
+        return True

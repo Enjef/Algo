ac2c3678ff411761efd2efbe52db3c8238212c09
Evgeniy <ferno1@yandex.ru>
2021-10-09 00:10:28 +0300

    dp day 5

--- Study Plan - Dynamic Programming/Day 5 maximum-subarray/53 - Maximum Subarray.py
+++ Study Plan - Dynamic Programming/Day 5 maximum-subarray/53 - Maximum Subarray.py
@@ -1,8 +1,16 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray(self, nums: List[int]) -> int:  # 63.58%  44.12%
         max_sub = float('-inf')
         cur_sub = float('-inf')
         for num in nums:
             cur_sub = max(cur_sub + num, num)
             max_sub = max(max_sub, cur_sub)
         return max_sub
+
+    def maxSubArray_dp(self, nums: List[int]) -> int:  # 12.46% 22.12%
+        dp = [-10001]*3
+        for num in nums:
+            dp[0], dp[1] = dp[1], max(num, dp[1]+num)
+            if dp[1] > dp[2]:
+                dp[2] = dp[1]
+        return dp[2]
--- Study Plan - Dynamic Programming/Day 5 maximum-subarray/918 - Maximum Sum Circular Subarray.py
+++ Study Plan - Dynamic Programming/Day 5 maximum-subarray/918 - Maximum Sum Circular Subarray.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:  # 13.75% 79.18%
         sum_max = cur_max = float('-inf')
         cur_min = sum_min = float('inf')
         all_negative = True
@@ -18,3 +18,23 @@ class Solution:
         if sum_min < 0:
             total -= sum_min
         return max(total, sum_max)
+
+    def maxSubarraySumCircular_dp(
+            self,
+            nums: List[int]) -> int:  # 54.32% 52.02%
+        total = sum(nums)
+        dp_min = [3*10**4+1] * 2
+        dp_max = [-3*10**4-1] * 2
+        all_negative = True
+        for num in nums:
+            if num > 0:
+                all_negative = False
+            dp_min[0] = min(num, num+dp_min[0])
+            dp_min[1] = min(dp_min[1], dp_min[0])
+            dp_max[0] = max(num, num+dp_max[0])
+            dp_max[1] = max(dp_max[1], dp_max[0])
+        if all_negative:
+            return max(nums)
+        if dp_min[1] < 0:
+            total -= dp_min[1]
+        return max(total, dp_max[1])

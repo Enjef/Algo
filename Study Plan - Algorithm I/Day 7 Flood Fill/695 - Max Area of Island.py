9fba74d1baa0904c0f46a7e4adaa85c93a77b1fc
Evgeniy <ferno1@yandex.ru>
2021-10-12 19:07:14 +0300

    algo day 7

--- Study Plan - Algorithm I/Day 7 Flood Fill/695 - Max Area of Island.py
+++ Study Plan - Algorithm I/Day 7 Flood Fill/695 - Max Area of Island.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:  # (ok)slow 21.65%
         out = 0
         visited = []
         n, m = len(grid), len(grid[0])
@@ -23,3 +23,25 @@ class Solution:
                 score = helper(i, j)
                 out = max(out, score)
         return out
+
+    def maxAreaOfIsland_stack(
+            self,
+            grid: List[List[int]]) -> int:  # 14.34% 80.16%
+        size_max = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if not grid[i][j]:
+                    continue
+                stack = [(i, j)]
+                size = 0
+                while stack:
+                    x, y = stack.pop()
+                    if (
+                            0 <= x < len(grid) and
+                            0 <= y < len(grid[0]) and
+                            grid[x][y] == 1):
+                        grid[x][y] = 0
+                        size += 1
+                        stack.extend([(x+1, y), (x-1, y), (x, y+1), (x, y-1)])
+                size_max = max(size_max, size)
+        return size_max
--- Study Plan - Algorithm I/Day 7 Flood Fill/733 - Flood Fill.py
+++ Study Plan - Algorithm I/Day 7 Flood Fill/733 - Flood Fill.py
@@ -24,3 +24,22 @@ class Solution:
         helper(image, sr, sc, newColor)
         return image
 
+    def floodFill_second_run(
+            self,
+            image: List[List[int]],
+            sr: int,
+            sc: int,
+            newColor: int) -> List[List[int]]:  # 72.15% 97.97%
+        target = image[sr][sc]
+        if target == newColor:
+            return image
+        stack = [(sr, sc)]
+        while stack:
+            i, j = stack.pop()
+            if (
+                    0 <= i < len(image) and
+                    0 <= j < len(image[0]) and
+                    image[i][j] == target):
+                image[i][j] = newColor
+                stack.extend([(i-1, j), (i+1, j), (i, j-1), (i, j+1)])
+        return image

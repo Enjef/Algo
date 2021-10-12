9d9da08819c558e15001a2cc0d32d17ff4df7999
Evgeniy <ferno1@yandex.ru>
2021-10-12 15:34:13 +0300

    algo day 6 hash variations

--- Study Plan - Algorithm I/Day 6 Longest Substring Without Repeating Characters/3 - Longest Substring Without Repeating Characters.py
+++ Study Plan - Algorithm I/Day 6 Longest Substring Without Repeating Characters/3 - Longest Substring Without Repeating Characters.py
@@ -1,5 +1,5 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def lengthOfLongestSubstring(self, s: str) -> int:  # 44.06% 53.55%
         if len(s) == 1:
             return 1
         max_s = ''
@@ -12,3 +12,16 @@ class Solution:
             if len(cur) > len(max_s):
                 max_s = cur
         return len(max_s)
+
+    def lengthOfLongestSubstring_hash(self, s: str) -> int:  # 80.32% 24.19%
+        if len(s) <= 1:
+            return len(s)
+        chars = {}
+        left = 0
+        res = 0
+        for right, char in enumerate(s):
+            if char in chars:
+                left = max(left, chars[char]+1)
+            chars[char] = right
+            res = max(res, right-left+1)
+        return res
--- Study Plan - Algorithm I/Day 6 Longest Substring Without Repeating Characters/567 - Permutation in String.py
+++ Study Plan - Algorithm I/Day 6 Longest Substring Without Repeating Characters/567 - Permutation in String.py
@@ -6,3 +6,23 @@ class Solution:
             if s_list == sorted(s2[i:i+k]):
                 return True
         return False
+
+    def checkInclusion_hash(self, s1: str, s2: str) -> bool:  # 99.99% 33.41%
+        if len(s1) > len(s2):
+            return False
+        abc = 'abcdefghijklmnopqrstuvwxyz'
+        s1_hash = {k: 0 for k in abc}
+        s2_hash = {k: 0 for k in abc}
+        for char in s1:
+            s1_hash[char] = s1_hash.get(char, 0) + 1
+        for i in range(len(s1)):
+            s2_hash[s2[i]] = s2_hash.get(s2[i], 0) + 1
+        if s1_hash == s2_hash:
+            return True
+        n = len(s1)
+        for i in range(n, len(s2)):
+            s2_hash[s2[i-n]] -= 1
+            s2_hash[s2[i]] += 1
+            if s1_hash == s2_hash:
+                return True
+        return False

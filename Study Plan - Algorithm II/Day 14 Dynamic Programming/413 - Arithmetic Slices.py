fcd14c87ffd0fb17e2aab747e30018e124221a3d
Evgeniy <ferno1@yandex.ru>
2021-11-06 22:53:20 +0300

    algo II day 14

--- Study Plan - Algorithm II/Day 14 Dynamic Programming/413 - Arithmetic Slices.py
+++ Study Plan - Algorithm II/Day 14 Dynamic Programming/413 - Arithmetic Slices.py
@@ -13,3 +13,18 @@ class Solution:
             else:
                 combo = False
         return count
+
+    def numberOfArithmeticSlices_v2(
+            self,
+            nums: List[int]) -> int:  # 99.73% 74.02%
+        if len(nums) < 3:
+            return 0
+        out = 0
+        j = 0
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                j += 1
+                out += j
+            else:
+                j = 0
+        return out
--- Study Plan - Algorithm II/Day 14 Dynamic Programming/5 - Longest Palindromic Substring.py
+++ Study Plan - Algorithm II/Day 14 Dynamic Programming/5 - Longest Palindromic Substring.py
@@ -9,3 +9,18 @@ class Solution:
                 if cur == cur[::-1]:
                     return cur
             win -= 1
+
+    def longestPalindrome_dp_slice(self, s: str) -> str:  # 23.36% 12.17%
+        n = len(s)
+        dp = [[0]*n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = 1
+        out = s[0]
+        for i in range(n):
+            for j in range(i-1, -1, -1):
+                if s[i] == s[j]:
+                    if i-j == 1 or dp[i-1][j+1]:
+                        dp[i][j] = 1
+                        if len(out) < len(s[j:i+1]):
+                            out = s[j:i+1]
+        return out

9919c25e65af86cb53e6d425855ae3258a95f489
Evgeniy <ferno1@yandex.ru>
2021-11-07 11:43:57 +0300

    algo II day 15

--- Study Plan - Algorithm II/Day 15 Dynamic Programming/91 - Decode Ways.py
+++ Study Plan - Algorithm II/Day 15 Dynamic Programming/91 - Decode Ways.py
@@ -9,3 +9,15 @@ class Solution:
             if s[i-2:i][0] != '0' and int(s[i-2:i]) < 27:
                 dp[i] += dp[i-2]
         return dp[-1]
+
+    def numDecodings_v2(self, s: str) -> int:  # 97.47% 95.80%
+        if s[0] == '0':
+            return 0
+        n = len(s)
+        dp = [1, 1]
+        for i in range(1, n):
+            dp[0], dp[1] = (
+                dp[1],
+                dp[1] * (0 < int(s[i])) + dp[0] * (9 < int(s[i-1:i+1]) < 27)
+            )
+        return dp[-1]

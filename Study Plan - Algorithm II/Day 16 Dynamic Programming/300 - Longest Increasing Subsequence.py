60ea413ebe88af8f3ca63a6b8de8ec09300a9f9b
Evgeniy <ferno1@yandex.ru>
2021-11-09 10:02:16 +0300

    Study Plan - Algorithm II\Day 16 Dynamic Programming

--- Study Plan - Algorithm II/Day 16 Dynamic Programming/300 - Longest Increasing Subsequence.py
+++ Study Plan - Algorithm II/Day 16 Dynamic Programming/300 - Longest Increasing Subsequence.py
@@ -9,3 +9,18 @@ class Solution:
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], 1 + dp[j])
         return max(dp)
+
+    def lengthOfLIS_bin_search(self, nums: List[int]) -> int:  # 85.67% 48.48%
+        sub = []
+        for num in nums:
+            if not sub or sub[-1] < num:
+                sub.append(num)
+            left, right = 0, len(sub)-1
+            while left < right:
+                mid = left + (right - left) // 2
+                if sub[mid] < num:
+                    left = mid + 1
+                else:
+                    right = mid
+            sub[left] = num
+        return len(sub)

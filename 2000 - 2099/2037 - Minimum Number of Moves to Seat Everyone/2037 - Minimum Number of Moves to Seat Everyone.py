1a610d5ecf933a812167770d76a84c8338df1314
Evgeniy <ferno1@yandex.ru>
2021-11-12 12:23:23 +0300

    2037

--- 2000 - 2099/2037 - Minimum Number of Moves to Seat Everyone/2037 - Minimum Number of Moves to Seat Everyone.py
+++ 2000 - 2099/2037 - Minimum Number of Moves to Seat Everyone/2037 - Minimum Number of Moves to Seat Everyone.py
@@ -6,3 +6,22 @@ class Solution:
         seats.sort()
         students.sort()
         return sum([abs(x-y) for x, y in zip(seats, students)])
+
+    def minMovesToSeat_best_speed(
+            self,
+            seats: List[int],
+            students: List[int]) -> int:
+        seats = sorted(seats)
+        students = sorted(students)
+        return sum(abs(seats[i] - students[i]) for i in range(len(seats)))
+
+    def minMovesToSeat_best_memory(
+            self,
+            seats: List[int],
+            students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        result = 0
+        for i in range(len(students)):
+            result = result + abs(seats[i]-students[i])
+        return result

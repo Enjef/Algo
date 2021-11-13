8c451d3b263d8af54c18bc20ed23fd607d9d35c8
Evgeniy <ferno1@yandex.ru>
2021-11-13 11:41:48 +0300

    202

--- 200 - 299/202 - Happy Number/202- Happy Number.py
+++ 200 - 299/202 - Happy Number/202- Happy Number.py
@@ -8,3 +8,25 @@ class Solution:
         for num in str(n):
             sum += int(num)*int(num)
         return self.isHappy(sum)
+
+    def isHappy_algo_day_21(self, n: int) -> bool:  # 96.28% 16.24%
+        mem = set([n])
+        while n not in [1, 7]:
+            new = []
+            for i in range(1, len(str(n))+1):
+                new.append(n % 10)
+                n //= 10
+            n = sum([x*x for x in new])
+            if n in mem:
+                return False
+            mem.add(n)
+        return True
+
+    def isHappy_best_speed(self, n: int) -> bool:
+        num_set = set()
+        while n != 1:
+            if n in num_set:
+                return False
+            num_set.add(n)
+            n = sum(int(i) ** 2 for i in str(n))
+        return True

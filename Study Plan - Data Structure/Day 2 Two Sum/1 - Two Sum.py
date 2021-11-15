a7eb0af6502b280016e2ef3a740b32fbfff884b6
Evgeniy <ferno1@yandex.ru>
2021-11-15 12:47:03 +0300

    ds day 2

--- Study Plan - Data Structure/Day 2 Two Sum/1 - Two Sum.py
+++ Study Plan - Data Structure/Day 2 Two Sum/1 - Two Sum.py
@@ -7,3 +7,14 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+    def twoSum_v_2(
+            self,
+            nums: List[int],
+            target: int) -> List[int]:  # 79.67% 41.60%
+        seen = {}
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [seen[target - nums[i]], i]
+            seen[nums[i]] = i
+        return
--- Study Plan - Data Structure/Day 2 Two Sum/88 - Merge Sorted Array.py
+++ Study Plan - Data Structure/Day 2 Two Sum/88 - Merge Sorted Array.py
@@ -3,7 +3,7 @@ class Solution:
             self,
             nums1: List[int],
             m: int,
-            nums2: List[int], n: int) -> None:
+            nums2: List[int], n: int) -> None:  # 75.29%
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -18,3 +18,27 @@ class Solution:
                 m -= 1
             else:
                 nums1[i] = nums2.pop()
+
+    def merge_v_2(
+            self,
+            nums1: List[int],
+            m: int,
+            nums2: List[int], n: int) -> None:  # 75.29% 96.27%
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if not nums2:
+            return
+        if not nums1:
+            nums1.extend(nums2)
+            return
+        j = m - 1
+        k = n - 1
+        for i in range(m+n-1,-1,-1):
+            if (nums1[j] < nums2[k] or j < 0) and k > -1:
+                nums1[i] = nums2[k]
+                k -= 1
+            else:
+                nums1[i] = nums1[j]
+                j -= 1
+        return

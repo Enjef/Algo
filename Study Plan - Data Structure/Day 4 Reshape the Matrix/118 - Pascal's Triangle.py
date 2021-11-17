7b0bdd2bddbf8c16c5d3b2e103051f08ccad3009
Evgeniy <ferno1@yandex.ru>
2021-11-17 12:03:03 +0300

    ds day 4

--- Study Plan - Data Structure/Day 4 Reshape the Matrix/118 - Pascal's Triangle.py
+++ Study Plan - Data Structure/Day 4 Reshape the Matrix/118 - Pascal's Triangle.py
@@ -1,5 +1,5 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+    def generate(self, numRows: int) -> List[List[int]]:  # 17.61% 81.94%
         out = [[1], [1, 1]]
         if numRows == 1:
             return [out[0]]
@@ -8,4 +8,15 @@ class Solution:
             for j in range(1, len(out[-2])):
                 out[-1].append(out[-2][j-1] + out[-2][j])
             out[-1].append(1)
+        return out
+
+    def generate_v2(self, numRows: int) -> List[List[int]]:  # 66.99% 81.94%
+        out = [[1]]
+        for _ in range(numRows-1):
+            new_row = [1]
+            prev = out[-1]
+            for i in range(1, len(prev)):
+                new_row.append(prev[i-1]+prev[i])
+            new_row.append(1)
+            out.append(new_row)
         return out

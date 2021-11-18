64f5109de208032d20c0b0c85eea38f5400b5963
Evgeniy <ferno1@yandex.ru>
2021-11-18 11:26:04 +0300

    ds day 5

--- Study Plan - Data Structure/Day 5 Valid Sudoku/36 - Valid Sudoku.py
+++ Study Plan - Data Structure/Day 5 Valid Sudoku/36 - Valid Sudoku.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:  # 12.09% 70.78%
         for row in range(0, 7, 3):
             for block in range(0, 7, 3):
                 square = []
@@ -15,4 +15,21 @@ class Solution:
             row = [x for x in board[i] if x.isdigit()]
             if len(col) != len(set(col)) or len(row) != len(set(row)):
                 return False
+        return True
+
+    def isValidSudoku_v2(self, board: List[List[str]]) -> bool: # 75.44% 70.78%
+        for i in range(9):
+            row = [x for x in board[i] if x.isdigit()]
+            if len(set(row)) != len(row):
+                return False
+        for col in zip(*board):
+            col = [x for x in col if x.isdigit()]
+            if len(set(col)) != len(col):
+                return False
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                block = board[i][j:j+3] + board[i+1][j:j+3] + board[i+2][j:j+3]
+                block = [x for x in block if x.isdigit()]
+                if len(set(block)) != len(block):
+                    return False
         return True
--- Study Plan - Data Structure/Day 5 Valid Sudoku/74 - Search a 2D Matrix.py
+++ Study Plan - Data Structure/Day 5 Valid Sudoku/74 - Search a 2D Matrix.py
@@ -2,7 +2,7 @@ class Solution:
     def searchMatrix(
             self,
             matrix: List[List[int]],
-            target: int) -> bool:
+            target: int) -> bool:  # 18.82% 35.37%
         left = 0
         right = len(matrix) * len(matrix[0])
         while left <= right:
@@ -17,5 +17,22 @@ class Solution:
             if matrix[row_mid][col_mid] > target:
                 right = mid - 1
             if matrix[row_mid][col_mid] < target:
+                left = mid + 1
+        return False
+
+    def searchMatrix_v2(
+            self,
+            matrix: List[List[int]],
+            target: int) -> bool:  # 68.72% 65.54%
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            i, j = divmod(mid, n)
+            if matrix[i][j] == target:
+                return True
+            if matrix[i][j] > target:
+                right = mid - 1
+            else:
                 left = mid + 1
         return False

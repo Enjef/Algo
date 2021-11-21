b7a348c62802ae44c9985e8342b4f9500893e6f7
Evgeniy <ferno1@yandex.ru>
2021-11-21 12:30:41 +0300

    83

--- 1 - 99/83 - Remove Duplicates from Sorted List/83 - Remove Duplicates from Sorted List.py
+++ 1 - 99/83 - Remove Duplicates from Sorted List/83 - Remove Duplicates from Sorted List.py
@@ -31,3 +31,45 @@ class Solution:
                     continue
             cur = cur.next
         return head
+
+    def deleteDuplicates_best_speed(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        seen = set()
+        dummy = ListNode()
+        dummy.next = head
+        node = head
+        prev = dummy
+        while node:
+            if node.val in seen:
+                prev.next = node.next
+                node = node.next
+                continue
+            seen.add(node.val)
+            prev = node
+            node = node.next
+        return dummy.next
+
+    def deleteDuplicates_2nd_best_speed(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        while cur:
+            if cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next 
+            else:
+                cur = cur.next 
+        return head
+
+    def deleteDuplicates_best_memory(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        curr = head
+        while curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head

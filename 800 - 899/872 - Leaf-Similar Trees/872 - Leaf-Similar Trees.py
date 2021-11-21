7f083f76555ee3d4c03f8abb86379e4d55a1e09a
Evgeniy <ferno1@yandex.ru>
2021-11-21 12:51:54 +0300

    872

--- 800 - 899/872 - Leaf-Similar Trees/872 - Leaf-Similar Trees.py
+++ 800 - 899/872 - Leaf-Similar Trees/872 - Leaf-Similar Trees.py
@@ -5,21 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def leafSimilar(
+    def leafSimilar_mock(
             self,
             root1: Optional[TreeNode],
-            root2: Optional[TreeNode]) -> bool:  # 5.35% 42.12%
-        return self.dfs(root1, []) == self.dfs(root2, [])
-
-    def dfs(self, root, arr):
-        if not root:
-            return arr
-        if not root.left and not root.right:
-            arr.append(root.val)
-            return arr
-        self.dfs(root.left, arr)
-        self.dfs(root.right, arr)
-        return arr
+            root2: Optional[TreeNode]) -> bool:  # 71.32% 41.89%
+        def leaf(root, out):
+            if not root.left and not root.right:
+                out.append(root.val)
+            if root.left:
+                leaf(root.left, out)
+            if root.right:
+                leaf(root.right, out)
+            return out
+        return leaf(root1, []) == leaf(root2, [])
 
     def leafSimilar_stack(
             self,
@@ -72,45 +70,14 @@ class Solution:
         if cur.right:
             stack.append(cur.right)
 
-    def compute_leaf_value_sequence(self, root: TreeNode, seq: List[int]):
-        if not root:
-            return
-        if not root.left and not root.right:
-            seq.append(root.val)
-        self.compute_leaf_value_sequence(root.left, seq)
-        self.compute_leaf_value_sequence(root.right, seq)
-
-    def leafSimilar_best_speed(self, root1: TreeNode, root2: TreeNode) -> bool:
-        seq1 = []
-        seq2 = []
-        self.compute_leaf_value_sequence(root1, seq1)
-        self.compute_leaf_value_sequence(root2, seq2)
-        return seq1 == seq2
-
     def leafSimilar_best_memory(
             self,
             root1: TreeNode,
             root2: TreeNode) -> bool:
-        def bfs(root):
-            stack = [(0, root)]
-            res = []
-            while stack:
-                color, root = stack.pop()
-                if root:
-                    if color and not root.left and not root.right:
-                        res.append(root.val)
-                    elif not color:
-                        stack.append((1, root))
-                        stack.append((0, root.right))
-                        stack.append((0, root.left))
-            return res
-
-        res1 = bfs(root1)
-        res2 = bfs(root2)
-
-        if len(res1) != len(res2):
-            return False
-        for i, j in zip(res1, res2):
-            if i != j:
-                return False
-        return True
+        def dfs(node):
+            if node == None:
+                return []
+            if node.left == None and node.right == None:
+                return [node.val]
+            return dfs(node.left) + dfs(node.right)
+        return dfs(root1) == dfs(root2)

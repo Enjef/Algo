be6c8d2ef021792c6491bcf1ee73de4c67514f2f
Evgeniy <ferno1@yandex.ru>
2021-11-23 11:18:08 +0300

    94

--- 1 - 99/94 - Binary Tree Inorder Traversal/94 - Binary Tree Inorder Traversal.py
+++ 1 - 99/94 - Binary Tree Inorder Traversal/94 - Binary Tree Inorder Traversal.py
@@ -8,13 +8,66 @@ class Solution:
     def __init__(self):
         self.map = []
 
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
+    def inorderTraversal(self, root: TreeNode) -> List[int]:  # 85.82% 47.11%
         if not root:
             return self.map
         self.inorderTraversal(root.left)
         self.map.append(root.val)
         return self.inorderTraversal(root.right)
+
+    def preorderTraversal_rec(
+            self,
+            root: Optional[TreeNode]) -> List[int]:  # 63.73% 75.82%
+        def dfs(root, out):
+            if not root:
+                return out
+            out.append(root.val)
+            if not root.left and not root.right:
+                return out
+            if root.left:
+                dfs(root.left, out)
+            if root.right:
+                dfs(root.right, out)
+            return out
+        return dfs(root, [])
+
+    def preorderTraversal_stack(
+            self,
+            root: Optional[TreeNode]) -> List[int]:  # 23.88% 75.82%
+        out = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            out.append(node.val)
+            stack.extend([node.right, node.left])
+        return out
+
+    def preorderTraversal_best_speed(
+            self,
+            root: Optional[TreeNode]) -> List[int]:
+        def traverse(root=root, nodes=[]):
+            if root is not None:
+                nodes.append(root.val)
+                traverse(root.left, nodes)
+                traverse(root.right, nodes)
+            return nodes
+        return traverse()
+
+    def preorderTraversal_best_memory(
+            self,
+            root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        stack = [root,]
+        out = []
+        while len(stack) > 0:
+            root = stack.pop()
+            if root is not None:
+                out.append(root.val)
+                if root.right is not None:
+                    stack.append(root.right)
+                if root.left is not None:
+                    stack.append(root.left)
+        return out

b2f91b08b2aabc1ce049530e34196ab99bce76c1
Evgeniy <ferno1@yandex.ru>
2021-11-23 16:50:05 +0300

    229

--- 200 - 299/229 - Majority Element II/229 - Majority Element II.py
+++ 200 - 299/229 - Majority Element II/229 - Majority Element II.py
@@ -9,6 +9,22 @@ class Solution:
                 out.append(el)
         return out
 
+    def majorityElement_best_speed(self, nums: List[int]) -> List[int]:
+        l = []
+        d = dict()
+        for n in nums:
+            if n in d:
+                d[n] += 1
+            else:
+                d[n] = 1
+        for n in d:
+            if d[n] > int(len(nums)/3):
+                l.append(n)
+        return l
+
+    def majorityElement_2nd_best_speed_new(self, nums: List[int]) -> List[int]:
+        return [k for k,v in Counter(nums).items() if v >len(nums)//3 ]
+
     def majorityElement_second_to_best_speed(
             self,
             nums: List[int]) -> List[int]:
@@ -19,3 +35,12 @@ class Solution:
             if x > int(len(nums)/3):
                 ans.append(el)
         return ans
+
+    def majorityElement_mock(
+            self,
+            nums: List[int]) -> List[int]:  # 30.11% 0.00%
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        n = len(nums) // 3
+        return [key for key, value in count.items() if value > n]

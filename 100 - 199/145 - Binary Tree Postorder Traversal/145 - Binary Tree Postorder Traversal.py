8489cf391175e1f686c1b9ee78eb4869ecce3c23
Evgeniy <ferno1@yandex.ru>
2021-11-23 13:06:36 +0300

    145

--- 100 - 199/145 - Binary Tree Postorder Traversal/145 - Binary Tree Postorder Traversal.py
+++ 100 - 199/145 - Binary Tree Postorder Traversal/145 - Binary Tree Postorder Traversal.py
@@ -22,3 +22,39 @@ class Solution(object):
             if root.right:
                 stack.append(root.right)
         return out[::-1]
+
+    def postorderTraversal_ds_day_10(
+            self,
+            root: Optional[TreeNode]) -> List[int]:  # 22.68% 98.93%
+        def dfs(root, out):
+            if not root:
+                return out
+            dfs(root.left, out)
+            dfs(root.right, out)
+            out.append(root.val)
+            return out
+        return dfs(root, [])
+
+    def postorderTraversal_best_speed(
+            self,
+            root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if not root:
+            return res
+        def postorder(node):
+            if node:
+                postorder(node.left)
+                postorder(node.right)
+                res.append(node.val)
+        postorder(root)
+
+    def postorderTraversal_3d_best_speed(
+            self,
+            root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        return (
+            self.postorderTraversal(root.left) +
+            self.postorderTraversal(root.right) +
+            [root.val]
+        )

2daf60e817060fc8fad829285c5c5bf53016a6b5
Evgeniy <ferno1@yandex.ru>
2021-11-24 18:26:04 +0300

    575

--- 500 - 599/575 - Distribute Candies/575 - Distribute Candies.py
+++ 500 - 599/575 - Distribute Candies/575 - Distribute Candies.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def distributeCandies(self, candyType):
+    def distributeCandies(self, candyType):  # 92.95% 87.18%
         """
         :type candyType: List[int]
         :rtype: int
@@ -9,3 +9,8 @@ class Solution(object):
         if len(c_set) >= half:
             return half
         return len(c_set)
+
+    def distributeCandies_mock(
+            self,
+            candyType: List[int]) -> int:  # 32.09% 65.77%
+        return min(len(candyType)//2, len(set(candyType)))

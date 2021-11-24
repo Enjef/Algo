0db4f45467b5074da447319cc85c49e2c71eca6d
Evgeniy <ferno1@yandex.ru>
2021-11-24 14:47:48 +0300

    102

--- 100 - 199/102 - Binary Tree Level Order Traversal/102 - Binary Tree Level Order Traversal.py
+++ 100 - 199/102 - Binary Tree Level Order Traversal/102 - Binary Tree Level Order Traversal.py
@@ -47,3 +47,54 @@ class Solution(object):
             count = cur_count
             results.append(row)
         return results
+
+    def levelOrder_mock_rec(
+            self,
+            root: Optional[TreeNode]) -> List[List[int]]:  # 24.98% 9.20%
+        def dfs(node, level, out):
+            if not node:
+                return out
+            if level > len(out):
+                out.append([node.val])
+            else:
+                out[level-1].append(node.val)
+            dfs(node.left, level+1, out)
+            return dfs(node.right, level+1, out)
+        return dfs(root, 1, [])
+
+    def levelOrder_mock_stack(
+            self,
+            root: Optional[TreeNode]) -> List[List[int]]:  # 15.21% 45.00%
+        if not root:
+            return root
+        stack = [(root, 1)]
+        out = []
+        while stack:
+            cur, level = stack.pop()
+            if len(out) < level:
+                out.append([cur.val])
+            else:
+                out[level-1].append(cur.val)
+            if cur.right:
+                stack.append((cur.right, level+1))
+            if cur.left:
+                stack.append((cur.left, level+1))
+        return out
+
+    def levelOrder_mock_stack_refactored(
+            self,
+            root: Optional[TreeNode]) -> List[List[int]]:  # 28.71% 87.41%
+        if not root:
+            return root
+        stack = [(root, 1)]
+        out = []
+        while stack:
+            cur, level = stack.pop()
+            if len(out) < level:
+                out.append([])
+            out[level-1].append(cur.val)
+            if cur.right:
+                stack.append((cur.right, level+1))
+            if cur.left:
+                stack.append((cur.left, level+1))
+        return out

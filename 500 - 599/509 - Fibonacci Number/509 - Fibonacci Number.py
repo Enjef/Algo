8a8d1fde57ec996c23644b7644992ee203c6deca
Evgeniy <ferno1@yandex.ru>
2021-11-24 18:23:22 +0300

    509

--- 500 - 599/509 - Fibonacci Number/509 - Fibonacci Number.py
+++ 500 - 599/509 - Fibonacci Number/509 - Fibonacci Number.py
@@ -1,14 +1,15 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        a, b = 0, 1
+    def fib(self, n: int) -> int:  # 85.26% 42.68%
+        a = 0
+        b = 1
         if n == 0:
-            return a
-        if n == 1:
-            return b
-        while n-1 > 0:
-            a, b = b, a + b
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        while n:
             n -= 1
-        return b
+            a, b = b, a + b
+        return a
 
     def fib_for(self, n: int) -> int:
         a, b = 0, 1
@@ -17,3 +18,16 @@ class Solution:
         for i in range(n):
             a, b = b, a + b
         return a
+
+    def fib_mock(self, n: int) -> int:  # 29.11% 1.00%
+        cache = {
+            0: 0,
+            1: 1
+        }
+        if n < 2:
+            return cache[n]
+        i = 2
+        while i <= n:
+            cache[i] = cache[i-1] + cache[i-2]
+            i += 1
+        return cache[i-1]

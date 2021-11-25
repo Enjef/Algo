dd8d1a394b22cd982ec55164f10f6b5f7e1ae2c8
Evgeniy <ferno1@yandex.ru>
2021-11-25 10:39:45 +0300

    112

--- 100 - 199/112 - Path Sum/112 - Path Sum.py
+++ 100 - 199/112 - Path Sum/112 - Path Sum.py
@@ -1,24 +1,40 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        curr_a, curr_b = headA, headB
-        while curr_a != curr_b:
-            curr_a = curr_a.next if curr_a else headB
-            curr_b = curr_b.next if curr_b else headA
-        return curr_a
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:  # 99.76% 1%
+        summ = 0
+        return self.summ_get(root, summ, targetSum)
 
-    def getIntersection_set(self, headA: ListNode, headB: ListNode) -> ListNode:
-        set_a = set()
-        while headA:
-            set_a.add(headA)
-            headA = headA.next
-        while headB:
-            if headB in set_a:
-                return headB
-            headB = headB.next
-        return None
+    def summ_get(self, root, summ, targetSum):
+        if not root:
+            return False
+        summ += root.val
+        if not root.left and not root.right:
+            if summ == targetSum:
+                return True
+        return (
+            self.summ_get(root.left, summ, targetSum) or
+            self.summ_get(root.right, summ, targetSum)
+        )
+
+    def hasPathSum_ds_day_11(
+            self,
+            root: Optional[TreeNode], targetSum: int) -> bool:  # 84.49% 99.53%
+        if not root:
+            return False
+        stack = [(root, 0)]
+        while stack:
+            node, cur = stack.pop()
+            if not node.left and not node.right:
+                if cur + node.val == targetSum:
+                    return True
+                continue
+            if node.left:
+                stack.append((node.left, cur+node.val))
+            if node.right:
+                stack.append((node.right, cur+node.val))
+        return False

4cf857de3139b85faecbfc7f1e7c98e26bf5a8c1
Evgeniy <ferno1@yandex.ru>
2021-11-25 10:47:01 +0300

    226

--- 200 - 299/226 - Invert Binary Tree/226 - Invert Binary Tree.py
+++ 200 - 299/226 - Invert Binary Tree/226 - Invert Binary Tree.py
@@ -43,3 +43,18 @@ class Solution:
                 self.invertTree(root.right), self.invertTree(root.left)
             )
         return root
+
+    def invertTree_ds_day_12(
+        self,
+        root: Optional[TreeNode]) -> Optional[TreeNode]:  # 5.08% 92.39%
+        if not root:
+            return root
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root

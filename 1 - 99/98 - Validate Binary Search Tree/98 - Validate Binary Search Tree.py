3c4ed009f2a25ccc6ace621333ec569836f54a6e
Evgeniy <ferno1@yandex.ru>
2021-11-27 12:00:38 +0300

    98

--- 1 - 99/98 - Validate Binary Search Tree/98 - Validate Binary Search Tree.py
+++ 1 - 99/98 - Validate Binary Search Tree/98 - Validate Binary Search Tree.py
@@ -13,3 +13,45 @@ class Solution:
                 [(cur.left, left, cur.val), (cur.right, cur.val, right)]
             )
         return True
+
+    def isValidBST_ds_day_14(
+            self,
+            root: Optional[TreeNode]) -> bool:  # 24.08% 54.01%
+        stack = [(root, float('-inf'), float('inf'))]
+        while stack:
+            cur, left, right = stack.pop()
+            if not cur:
+                continue
+            if not(left<cur.val<right):
+                return False
+            stack.extend(
+                [(cur.right, cur.val, right), (cur.left, left, cur.val)]
+            )
+        return True
+
+    def isValidBST_best_speed(self, root: Optional[TreeNode]) -> bool:
+        left = self.validTree(root.left, -sys.maxsize, root.val)
+        right = self.validTree(root.right, root.val, sys.maxsize)
+        return left and right
+
+    def validTree(self, root, low, high):
+        if not root:
+            return True
+        if root.val <= low or root.val >= high:
+            return False
+        left = self.validTree(root.left, low, root.val)
+        right = self.validTree(root.right, root.val, high)
+        return left and right
+
+    def isValidBST_2d_best_speed(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, lower = float('-inf'), upper = float('inf')):
+            if not node:
+                return True
+            if node.val <= lower or node.val >=upper :
+                return False
+            if not helper(node.right, node.val, upper):
+                return False
+            if not helper(node.left, lower,node.val):
+                return False
+            return True
+        return helper(root)

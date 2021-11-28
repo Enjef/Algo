645b42a5aa3defa913592e5833f54f960ca8e537
Evgeniy <ferno1@yandex.ru>
2021-11-28 22:55:51 +0300

    237

--- 200 - 299/237 - Delete Node in a Linked List/237 - Delete Node in a Linked List.py
+++ 200 - 299/237 - Delete Node in a Linked List/237 - Delete Node in a Linked List.py
@@ -5,7 +5,31 @@
 #         self.next = None
 
 class Solution(object):
-    def deleteNode(self, node):
+    def deleteNode(self, node):  # 67.31% 62.98%
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+    def deleteNode_mock(self, node):  # 96.43% 62.98%
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        while node.next and node.next.next:
+            node.val = node.next.val
+            node = node.next
+        node.val = node.next.val
+        node.next = None
+
+    def deleteNode_best_speed(self, node):
+        nextNode = node.next
+        node.val = nextNode.val
+        node.next = nextNode.next
+
+    def deleteNode_best_memory(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.

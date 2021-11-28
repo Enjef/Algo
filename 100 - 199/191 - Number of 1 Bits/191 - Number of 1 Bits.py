9df2b919286f66859b476251d248ada4dff71c73
Evgeniy <ferno1@yandex.ru>
2021-11-28 22:59:43 +0300

    191

--- 100 - 199/191 - Number of 1 Bits/191 - Number of 1 Bits.py
+++ 100 - 199/191 - Number of 1 Bits/191 - Number of 1 Bits.py
@@ -1,3 +1,6 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
+    def hammingWeight(self, n: int) -> int:  # 68.68% 5.53%
         return str(bin(n)).count('1')
+
+    def hammingWeight_mock(self, n: int) -> int:  # 96.91% 89.17%
+        return str(bin(int(n))).count('1')

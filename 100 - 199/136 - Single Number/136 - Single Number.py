ba514943221428ba19a605e8271f520d0756d040
Evgeniy <ferno1@yandex.ru>
2021-11-28 16:55:49 +0300

    136

--- 100 - 199/136 - Single Number/136 - Single Number.py
+++ 100 - 199/136 - Single Number/136 - Single Number.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def singleNumber(self, nums):
+    def singleNumber(self, nums):  # 64.33% 13.97%
         """
         :type nums: List[int]
         :rtype: int
@@ -11,3 +11,21 @@ class Solution(object):
             else:
                 count.pop(num)
         return count.keys()[0]
+
+    def singleNumber_ds_2_v1(self, nums: List[int]) -> int:  # 85.04% 96.80%
+        res = 0
+        for num in nums:
+            res = res ^ num
+        return res
+
+    def singleNumber_ds_2_v2(self, nums: List[int]) -> int:  # 99.99% 60.93%
+        cart = set()
+        for num in nums:
+            if num in cart:
+                cart.remove(num)
+            else:
+                cart.add(num)
+        return list(cart)[0]
+
+    def singleNumber_best_speed(self, nums):
+        return 2 * sum(set(nums)) - sum(nums)

3b5499d67b1ed70da0cd3779390c759257766e45
Evgeniy <ferno1@yandex.ru>
2021-11-28 18:38:59 +0300

    15

--- 1 - 99/15 - 3Sum/15 - 3Sum.py
+++ 1 - 99/15 - 3Sum/15 - 3Sum.py
@@ -18,3 +18,18 @@ class Solution:
                     temp = tuple(sorted([i, j, k]))
                     out.add(temp)
         return out
+
+    def threeSum_ds_2_day_1(
+            self,
+            nums: List[int]) -> List[List[int]]:  # 19.87% 26.58%
+        diff = set()
+        n = len(nums)
+        out = set()
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if -(nums[i]+nums[j]) in diff:
+                    out.add(
+                        tuple(sorted([-nums[i]-nums[j], nums[i], nums[j]]))
+                    )
+            diff.add(nums[i])
+        return out

83f29b3bb44e3c084d59173aad71c36807dcb755
Evgeniy <ferno1@yandex.ru>
2021-12-04 13:57:32 +0300

    763

--- 700 - 799/763 - Partition Labels/763 - Partition Labels.py
+++ 700 - 799/763 - Partition Labels/763 - Partition Labels.py
@@ -32,3 +32,39 @@ class Solution:
                 cur_start = i + 1
                 cur_end = i + 1
         return ans
+
+    def partitionLabels_ds_day_7(self, s: str) -> List[int]:  # 99.37% 56.71%
+        coords = {}
+        for i in range(len(s)):
+            if s[i] in coords:
+                coords[s[i]][1] = i
+            else:
+                coords[s[i]] = [i, i]
+        parts = sorted(coords.values())
+        out = [parts[0][1]-parts[0][0]+1]
+        cur = parts[0][1]
+        for i in range(1, len(parts)):
+            if parts[i][1] < cur:
+                continue
+            elif parts[i][0] < cur:
+                out[-1] += parts[i][1] - cur
+                cur = parts[i][1]
+            else:
+                out.append(parts[i][1] - cur)
+                cur = parts[i][1]
+        return out
+
+    def partitionLabels_best_memory(self, s: str) -> List[int]:
+        ret = []
+        d = {}
+        for i in range(len(s)):
+            d[s[i]] = i
+        end = 0
+        count = 0
+        for i in range(len(s)):
+            end = max(end, d[s[i]])
+            count += 1 
+            if i == end:
+                ret.append(count)
+                count = 0
+        return ret

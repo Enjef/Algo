d817231f7d1b0483f94c5c53ab7fabb0d930259e
Evgeniy <ferno1@yandex.ru>
2021-12-04 13:54:42 +0300

    290

--- 200 - 299/290 - Word Pattern/290 - Word Pattern.py
+++ 200 - 299/290 - Word Pattern/290 - Word Pattern.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def wordPattern(self, pattern, s):
+    def wordPattern(self, pattern, s):  # 77.41% 47.14%
         """
         :type pattern: str
         :type s: str
@@ -15,5 +15,36 @@ class Solution(object):
                     return False
                 char_map[str(pattern[i])] = s[i]
             elif char_map[str(pattern[i])] != s[i]:
+                return False
+        return True
+
+    def wordPattern_mock(self, pattern: str, s: str) -> bool:  # 95.51% 99.37%
+        s = s.split()
+        vals = set()
+        if len(pattern) != len(s):
+            return False
+        check = {}
+        for i in range(len(s)):
+            if pattern[i] in check and check[pattern[i]] != s[i]:
+                return False
+            if s[i] in vals and pattern[i] not in check:
+                return False
+            check[pattern[i]] = s[i]
+            vals.add(s[i])
+        return True
+
+    def wordPattern_best_speed(self, pattern: str, s: str) -> bool:
+        a = s.split()
+        if len(pattern) != len(a):
+            return False
+        d, d2 = {}, {}
+        for c, w in zip(pattern, a):
+            if c not in d:
+                d[c] = w
+            elif d[c] != w:
+                return False
+            if w not in d2:
+                d2[w] = c
+            elif d2[w] != c:
                 return False
         return True

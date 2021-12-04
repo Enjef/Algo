434ff70158a905fc1249b2315c88187373d6a16c
Evgeniy <ferno1@yandex.ru>
2021-12-04 17:49:37 +0300

    482

--- 400 - 499/482 - License Key Formatting/482 - License Key Formatting.py
+++ 400 - 499/482 - License Key Formatting/482 - License Key Formatting.py
@@ -1,5 +1,5 @@
 class Solution:
-    def licenseKeyFormatting(self, s: str, k: int) -> str:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:  # 40.50% 97.68%
         s = s.replace('-', '').upper()
         if len(s) < k:
             return s
@@ -13,4 +13,43 @@ class Solution:
             size -= k
         if size:
             out = s[:size] + '-' + out
+        return out
+
+    def licenseKeyFormatting_mock(
+            self,
+            s: str,
+            k: int) -> str:  # 47.42% 78.01%
+        out = ''
+        dash = 0
+        for i, char in enumerate(s[::-1]):
+            if char == '-':
+                continue
+            out += char.upper()
+            if (len(out) - dash) % k == 0:
+                out += '-'
+                dash += 1
+        if out and out[-1] == '-':
+            return out[:-1][::-1]
+        return out[::-1]
+
+    def licenseKeyFormatting_mock_v2(
+            self,
+            s: str,
+            k: int) -> str:  # 69.05% 59.50%
+        out = ''
+        s = s.replace('-', '').upper()
+        for index in range(len(s)%k):
+            out = out + s[index]
+        if not out:
+            index = -1
+            dash = k
+        else:
+            index = len(out) - 1
+            dash = 0
+        for i in range(index+1, len(s)):
+            if not dash:
+                out = out + '-'
+                dash = k
+            out = out + s[i]
+            dash = dash - 1
         return out

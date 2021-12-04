97c968bbe1139dc7270107053ad1ddad1796297d
Evgeniy <ferno1@yandex.ru>
2021-12-04 21:40:33 +0300

    9

--- 1 - 99/9 - Palindrome Number/9 - Palindrome Number.py
+++ 1 - 99/9 - Palindrome Number/9 - Palindrome Number.py
@@ -1,6 +1,3 @@
-import math
-
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x == 0:
@@ -16,6 +13,15 @@ class Solution:
             j -= 1
         return True
 
+    def isPalindrome_best_speed(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
 
-x = Solution()
-print(x.isPalindrome(0))
+    def isPalindrome_7th_best_speed(self, x: int) -> bool:
+        if x < 0:
+            return False
+        number = x
+        reverse = 0
+        while number:
+            reverse = reverse * 10 + number % 10
+            number //= 10
+        return x == reverse

443d375839f76a999a67a7b0e167d0945aba927a
Evgeniy <ferno1@yandex.ru>
2021-12-06 00:44:24 +0300

    11

--- 1 - 99/11 - Container With Most Water/11 - Container With Most Water.py
+++ 1 - 99/11 - Container With Most Water/11 - Container With Most Water.py
@@ -24,6 +24,24 @@ class Solution:
                 j -= 1
         return v_max
 
+    def maxArea_best_speed_new(self, height) -> int:
+        def area(left, right):
+            w = right - left
+            h = min(height[left], height[right])
+            return h * w
+        max_area = 0
+        left_pointer, right_pointer = 0, len(height) - 1
+        max_height = max(height)
+        while left_pointer < right_pointer:
+            max_area = max(max_area, area(left_pointer, right_pointer))
+            if max_height * (right_pointer - left_pointer) < max_area:
+                break
+            if height[left_pointer] < height[right_pointer]:
+                left_pointer += 1
+            else:
+                right_pointer -= 1
+        return max_area
+
     def maxArea_best_speed(self, height: List[int]) -> int:
         res = 0
         r = len(height) - 1

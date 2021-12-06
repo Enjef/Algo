fdcd6b32015253f6634fc51454f036400b0c89df
Evgeniy <ferno1@yandex.ru>
2021-12-06 13:19:36 +0300

    345

--- 300 - 399/345 - Reverse Vowels of a String/345 - Reverse Vowels of a String.py
+++ 300 - 399/345 - Reverse Vowels of a String/345 - Reverse Vowels of a String.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverseVowels(self, s):
+    def reverseVowels(self, s):  # 63.82% 37.14%
         """
         :type s: str
         :rtype: str
@@ -16,3 +16,53 @@ class Solution:
             i += 1
             j -= 1
         return "".join(s)
+
+    def reverseVowels_mock(self, s: str) -> str:  # 95.07% 49.15%
+        vowels = set('aeiouAEIOU')
+        chars = list(s)
+        left, right = 0, len(s) - 1
+        while left <= right:
+            if chars[left] not in vowels and chars[right] not in vowels:
+                left += 1
+                right -= 1
+            elif chars[left] in vowels and chars[right] in vowels:
+                chars[left], chars[right] = chars[right], chars[left]
+                left += 1
+                right -= 1
+            elif chars[left] in vowels:
+                right -= 1
+            else:
+                left += 1
+        return ''.join(chars)
+
+    def reverseVowels_best_speed(self, s: str) -> str:
+        s = list(s)
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < len(s) and s[l] not in "aeiouAEIOU":
+                l += 1
+            while r > 0 and s[r] not in "aeiouAEIOU":
+                r -= 1  
+            if l < r:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+        return ''.join(s)
+
+    def reverseVowels_best_memory(self, s: str) -> str:
+        vowelList = ("a", "e", "i", "o", "u")
+        vowelSeen = []
+        for letter in s:
+            if letter.lower() in vowelList:
+                vowelSeen.append(letter)
+        if len(vowelSeen) == 0:
+            return s
+        else:
+            output = ""
+            for letter in s:
+                if letter.lower() in vowelList:
+                    output = output + vowelSeen.pop()
+                else:
+                    output = output + letter
+        return output

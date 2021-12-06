087abd654e0a2c84813d8fe2fbee50f9f4a67e43
Evgeniy <ferno1@yandex.ru>
2021-12-06 13:34:40 +0300

    515

---
+++ 500 - 599/515 - Find Largest Value in Each Tree Row/515 - Find Largest Value in Each Tree Row.py
@@ -0,0 +1,70 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues_mock(
+            self,
+            root: Optional[TreeNode]) -> List[int]:  # 34.89% 57.73%
+        out = []
+        stack = [root]
+        while stack:
+            next_level = []
+            level_max = float('-inf')
+            while stack:
+                cur = stack.pop()
+                if not cur:
+                    continue
+                level_max = max(level_max, cur.val)
+                next_level.extend([cur.left, cur.right])
+            out.append(level_max)
+            stack = next_level
+        out.pop()
+        return out
+
+    def largestValues_mock_v2(
+            self,
+            root: Optional[TreeNode]) -> List[int]:  # 94.56% 57.73%
+        if not root:
+            return root
+        out = []
+        stack = [root]
+        while stack:
+            next_level = []
+            level_max = float('-inf')
+            while stack:
+                cur = stack.pop()
+                if not cur:
+                    continue
+                level_max = max(level_max, cur.val)
+                if cur.left:
+                    next_level.append(cur.left)
+                if cur.right:
+                    next_level.append(cur.right)
+            out.append(level_max)
+            stack = next_level
+        return out
+
+    def largestValues_best_speed(
+           self,
+           root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        queue = deque()
+        queue.append(root)
+        result = []
+        while queue:
+            levelMax = float('-inf')
+            levelSize = len(queue)
+            for i in range(levelSize):
+                node = queue.popleft()
+                if node:
+                    levelMax = max(levelMax,node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+            result.append(levelMax)
+        return result             

1640a4ba86755e64671cf8c535a4edf4dd06eef8
Evgeniy <ferno1@yandex.ru>
2021-12-08 23:24:38 +0300

    82

--- 1 - 99/82 - Remove Duplicates from Sorted List II/82 - Remove Duplicates from Sorted List II.py
+++ 1 - 99/82 - Remove Duplicates from Sorted List II/82 - Remove Duplicates from Sorted List II.py
@@ -4,29 +4,52 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-        unique = True
-        last_unique = None
-        first = head
-        second = first.next
-        while second:
-            if second.val == first.val:
-                second = second.next
-                unique = False
-                continue
-            elif second.val != first.val and not unique:
-                first = second
-                second = second.next
-                continue
+    def deleteDuplicates(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:  # 92.65% 59.04%
+        first = prev = ListNode(next=head)
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
             else:
-                unique = True
-                if not last_unique:
-                    last_unique = first
-                    head = last_unique
-                    second = second.next
-                else:
-                    last_unique.next = first
-                    second = second.next
-        return head
+                prev = prev.next
+                head = head.next
+        return first.next
+
+    def deleteDuplicates_best_speed(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        fake = ListNode(0)
+        fake.next = head
+        pre = fake
+        cur = head
+        while cur:
+            while cur and cur.next and cur.val == cur.next.val:
+                cur = cur.next
+            if(pre.next == cur):
+                pre = pre.next
+            else:
+                pre.next = cur.next
+            cur = cur.next
+        return fake.next
+
+    def deleteDuplicates_best_memory(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = {}
+        root = head
+        while root != None:
+            if root.val not in nums:
+                nums[root.val] = 0
+            nums[root.val] += 1
+            root = root.next
+        root = ListNode(0, None)
+        current = root
+        for i in nums.keys():
+            if nums[i] == 1:
+                current.next = ListNode(i, None)
+                current = current.next
+        return root.next

1278374914eec9c4c687bf5d3d58c72882ae8976
Evgeniy <ferno1@yandex.ru>
2021-12-09 13:36:31 +0300

    707

---
+++ 700 - 799/707 - Design Linked List/707 - Design Linked List.py
@@ -0,0 +1,37 @@
+class MyLinkedList:
+
+    def __init__(self):  # 99.14% 91.22%
+        self.list = []
+
+    def get(self, index: int) -> int:
+        if not(0 <= index <= len(self.list)-1):
+            return -1
+        return self.list[index]
+
+    def addAtHead(self, val: int) -> None:
+        self.list.insert(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        self.list.append(val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index == len(self.list):
+            self.list.append(val)
+            return
+        if not(0 <= index <= len(self.list)-1):
+            return
+        self.list.insert(index, val)
+
+    def deleteAtIndex(self, index: int) -> None:
+        if not(0 <= index <= len(self.list)-1):
+            return
+        self.list.pop(index)
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)

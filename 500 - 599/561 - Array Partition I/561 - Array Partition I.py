b7bd8a2ba2f7c3b9e3c2d2c95908bf1bb679ec30
Evgeniy <ferno1@yandex.ru>
2021-12-10 02:06:49 +0300

    561

--- 500 - 599/561 - Array Partition I/561 - Array Partition I.py
+++ 500 - 599/561 - Array Partition I/561 - Array Partition I.py
@@ -1,12 +1,19 @@
 #  slow 9.3%
 class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
+    def arrayPairSum(self, nums: List[int]) -> int:  # 31.86% 00.00%
         nums.sort()
         summ = 0
         for i in range(0, len(nums), 2):
             summ += min(nums[i], nums[i+1])
         return summ
 
-    def arrayPairSum_short(self, nums: List[int]) -> int:
+    def arrayPairSum_short(self, nums: List[int]) -> int:  # 95.20% 90.82%
         nums.sort()
         return sum(nums[::2])
+
+    def arrayPairSum_mock(self, nums: List[int]) -> int:  # 54.28% 70.60%
+        nums.sort()
+        out = 0
+        for i in range(0, len(nums), 2):
+            out += nums[i]
+        return out

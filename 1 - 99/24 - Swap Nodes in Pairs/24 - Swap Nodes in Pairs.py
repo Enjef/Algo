a077e40d2467cf234ca29ef7a71e563d322021ba
Evgeniy <ferno1@yandex.ru>
2021-12-09 13:00:16 +0300

    24

--- 1 - 99/24 - Swap Nodes in Pairs/24 - Swap Nodes in Pairs.py
+++ 1 - 99/24 - Swap Nodes in Pairs/24 - Swap Nodes in Pairs.py
@@ -50,3 +50,47 @@ class Solution:
             else:
                 break
         return head
+
+    def swapPairs_ds2_day_12(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:  # 67.75% 17.37%
+        if not head or not head.next:
+            return head
+        dummy = prev = ListNode(next=head.next)
+        while head and head.next:
+            next_prev = head
+            next_first = head.next.next
+            cur_second = head.next
+            prev.next = cur_second
+            cur_second.next = head
+            head.next = next_first
+            prev = next_prev
+            head = next_first
+        return dummy.next
+
+    def swapPairs_best_speed(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, cur = dummy, head
+        while cur and cur.next:
+            nxtPair = cur.next.next
+            scd = cur.next
+            cur.next = nxtPair
+            scd.next = cur
+            prev.next = scd
+            prev = cur
+            cur = nxtPair
+        return dummy.next 
+
+    def swapPairs_best_memory(
+            self,
+            head: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode(next = head)
+        pre = res
+        while pre.next and pre.next.next:
+            cur, post = pre.next, pre.next.next            
+            cur.next, post.next = post.next, cur
+            pre.next = post
+            pre = pre.next.next
+        return res.next

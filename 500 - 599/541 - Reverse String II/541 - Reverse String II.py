3e704d25e680e9b830bfb9f23c06c46a4f86639d
Evgeniy <ferno1@yandex.ru>
2021-12-10 20:23:55 +0300

    541 style

--- 500 - 599/541 - Reverse String II/541 - Reverse String II.py
+++ 500 - 599/541 - Reverse String II/541 - Reverse String II.py
@@ -20,3 +20,18 @@ class Solution:
         for i in range(0, len(s), 2*k):
             s = s[:i] + s[i:i+k][::-1] + s[i+k:]
         return s
+
+    def reverseStr_mock(self, s: str, k: int) -> str:  # 78.70% 73.50%
+        out = ''
+        flag = True
+        for i in range(0, len(s), k):
+            if flag:
+                out += s[i: i+k][::-1]
+                flag = False
+            else:
+                out += s[i: i+k]
+                flag = True
+        if len(out) < len(s):
+            out += s[i: i+len(s)-len(out)]
+        return out
+        

3a2717680a50b426143fc903445fce0d6238fd1c
Evgeniy <ferno1@yandex.ru>
2021-12-11 18:59:57 +0300

    1122

--- 1100 - 1199/1122 - Relative Sort Array/1122 - Relative Sort Array.py
+++ 1100 - 1199/1122 - Relative Sort Array/1122 - Relative Sort Array.py
@@ -24,3 +24,23 @@ class Solution:
         for k, v in sorted(d.items()):
             ans += [k]*v
         return ans
+
+class Solution:
+    def relativeSortArray_mock(
+            self,
+            arr1: List[int],
+            arr2: List[int]) -> List[int]:  # 39.45% 89.55%
+        count = {}
+        last = []
+        first = []
+        set_arr2 =set(arr2)
+        for num in arr1:
+            if num in set_arr2:
+                count[num] = count.get(num, 0) + 1
+            else:
+                last.append(num)
+        for num in arr2:
+            first.extend([num]*count[num])
+        last.sort()
+        arr1[:] = first + last
+        return arr1

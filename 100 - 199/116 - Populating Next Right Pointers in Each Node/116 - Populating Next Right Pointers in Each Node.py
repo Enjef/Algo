f50527b247a9220a1bc401111e8e74b6b4682254
Evgeniy <ferno1@yandex.ru>
2022-02-01 16:22:08 +0300

    116

--- 100 - 199/116 - Populating Next Right Pointers in Each Node/116 - Populating Next Right Pointers in Each Node.py
+++ 100 - 199/116 - Populating Next Right Pointers in Each Node/116 - Populating Next Right Pointers in Each Node.py
@@ -20,6 +20,23 @@ class Solution:
                 level[i].next = level[i+1]
         return root
 
+    def connect_mock(self, root) -> 'Optional[Node]':  # 95.73% 34.93%
+        if not root:
+            return root
+        stack = [root]
+        while stack:
+            temp = []
+            while stack:
+                cur = stack.pop(0)
+                if cur.left:
+                    temp.append(cur.left)
+                if cur.right:
+                    temp.append(cur.right)
+            for i in range(len(temp)-1):
+                temp[i].next = temp[i+1]
+            stack = temp
+        return root
+
     def connect_best_speed(self, root: 'Node') -> 'Node':
         if not root or not root.left:
             return root
@@ -44,4 +61,23 @@ class Solution:
                     queue.append(node.right)
                 if node.left:
                     queue.append(node.left)
+        return root
+
+    def connect_best_speed_new(self, root) -> 'Optional[Node]':
+        if not root:
+            return root
+        q = deque()
+        q.appendleft(root)
+        while q:
+            sz = len(q)
+            prev = None
+            for _ in range(sz):
+                curr = q.pop()
+                if prev:
+                    prev.next = curr
+                if curr.left:
+                    q.appendleft(curr.left)
+                if curr.right:
+                    q.appendleft(curr.right)
+                prev = curr
         return root

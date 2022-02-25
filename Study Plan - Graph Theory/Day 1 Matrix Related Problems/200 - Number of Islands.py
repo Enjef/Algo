4b1d21439ba0eef150188b38fa5e1d7ed8fae57a
Evgeniy <ferno1@yandex.ru>
2022-02-26 02:24:04 +0300

    Study Plan - Graph Theory day 1

---
+++ Study Plan - Graph Theory/Day 1 Matrix Related Problems/200 - Number of Islands.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int: # 57.59% 81.06%
+        def bfs(y, x):
+            if not(-1<y<m and -1<x<n and grid[y][x] == '1'):
+                return
+            grid[y][x] = '0'
+            for idx in range(4):
+                bfs(y+y_dir[idx], x+x_dir[idx])
+            return
+        
+        m, n, count = len(grid), len(grid[0]), 0
+        x_dir, y_dir = [0, 0, -1, 1], [-1, 1, 0, 0]
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    bfs(i, j)
+                    count += 1
+        return count
---
+++ Study Plan - Graph Theory/Day 1 Matrix Related Problems/733 - Flood Fill.py
@@ -0,0 +1,16 @@
+class Solution:
+    def floodFill(self, image, sr, sc, newColor): # 72.12% 43.71%
+        def bfs(y, x, target):
+            if not(-1<y<m and -1<x<n and image[y][x] == target):
+                return
+            image[y][x] = newColor
+            for idx in range(4):
+                bfs(y+y_dir[idx], x+x_dir[idx], target)
+            return
+        
+        m, n = len(image), len(image[0])
+        x_dir, y_dir = [0, 0, -1, 1], [-1, 1, 0, 0]
+        if image[sr][sc] == newColor:
+            return image
+        bfs(sr, sc, image[sr][sc])
+        return image

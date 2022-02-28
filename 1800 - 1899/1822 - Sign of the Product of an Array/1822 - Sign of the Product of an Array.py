9a26e7e208499ad1d3cc4ee98f25c1e817fe08e2
Evgeniy <ferno1@yandex.ru>
2022-02-28 17:18:29 +0300

    1822

--- 1800 - 1899/1822 - Sign of the Product of an Array/1822 - Sign of the Product of an Array.py
+++ 1800 - 1899/1822 - Sign of the Product of an Array/1822 - Sign of the Product of an Array.py
@@ -20,3 +20,33 @@ class Solution(object):
             if x < 0:
                 total *= -1
         return total
+
+    def arraySign_best_speed(self, nums: list[int]) -> int:
+        ans = 1
+        for n in nums:
+            if n== 0:
+                return 0
+            if n < 0:
+                ans*= -1
+        return ans  
+
+    def arraySign_2nd_best_speed(self, nums: List[int]) -> int:
+        neg_count = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                return 0
+            if nums[i] < 0:
+                neg_count += 1
+        
+        if neg_count % 2 == 0:
+            return 1
+        else: 
+            return -1
+
+    def arraySign_best_memory(self, nums: List[int]) -> int:
+        sign = 1
+        for num in nums:
+            if num == 0:
+                return 0
+            sign = sign*(-1) if num < 0 else sign
+        return sign

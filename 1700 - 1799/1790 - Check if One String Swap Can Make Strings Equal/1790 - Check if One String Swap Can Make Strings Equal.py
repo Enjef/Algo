f2f6794d892ba180b02241beb7bb7e2006a5ede3
Evgeniy <ferno1@yandex.ru>
2022-02-28 17:02:19 +0300

    1790

--- 1700 - 1799/1790 - Check if One String Swap Can Make Strings Equal/1790 - Check if One String Swap Can Make Strings Equal.py
+++ 1700 - 1799/1790 - Check if One String Swap Can Make Strings Equal/1790 - Check if One String Swap Can Make Strings Equal.py
@@ -15,6 +15,24 @@ class Solution:
                     return True
         return False
 
+    def areAlmostEqual_sp_day4(self, s1: str, s2: str) -> bool: # 94.51% 58.84%
+        if s1 == s2:
+            return True
+        diff = None
+        found = False
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                if diff:
+                    if found:
+                        return False
+                    if [s1[i], s2[i]] == diff:
+                        found = True
+                    else:
+                        return False
+                else:
+                    diff = [s2[i], s1[i]]
+        return found
+
     def areAlmostEqual_s_best(self, s1: str, s2: str) -> bool:
         a1, a2 = [], []
         for i in range(len(s1)):

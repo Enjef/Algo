e5bedf4ea94968f7fcab314f411d7a4d18e1d8a8
Evgeniy <ferno1@yandex.ru>
2022-03-04 21:38:50 +0300

    1672

--- 1600 - 1699/1672 - Richest Customer Wealth/1672 - Richest Customer Wealth.py
+++ 1600 - 1699/1672 - Richest Customer Wealth/1672 - Richest Customer Wealth.py
@@ -1,3 +1,10 @@
 class Solution(object):
-    def maximumWealth(self, accounts):
+    def maximumWealth(self, accounts):  # 92.69% 24.55%
         return max([sum(x) for x in accounts])
+
+    def maximumWealth_study_plan(self, accounts):  # 74.95% 81.46%
+        return sum(max(accounts, key=lambda x: sum(x)))
+
+    def maximumWealth_best_speed(self, accounts: List[List[int]]) -> int:
+        new_list = max([*map(sum, accounts)])
+        return new_list

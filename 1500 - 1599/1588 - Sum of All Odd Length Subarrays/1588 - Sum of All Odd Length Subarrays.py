64b013922ba097f6b29cc70b596dcd009dbe8d2a
Evgeniy <ferno1@yandex.ru>
2022-03-04 19:23:39 +0300

    1588

--- 1500 - 1599/1588 - Sum of All Odd Length Subarrays/1588 - Sum of All Odd Length Subarrays.py
+++ 1500 - 1599/1588 - Sum of All Odd Length Subarrays/1588 - Sum of All Odd Length Subarrays.py
@@ -5,3 +5,32 @@ class Solution(object):
             for j in range(0, len(arr)-i+1):
                 out += sum(arr[j:j+i])
         return out
+
+    def sumOddLengthSubarrays_v2(self, arr: List[int]) -> int:  # 69.27% 81.26%
+        out = sum(arr)
+        n = len(arr)
+        win = 3
+        while win <= n+1:
+            for i in range(n-win+1):
+                out += sum(arr[i:i+win])
+            win += 2
+        return out
+
+    def sumOddLengthSubarrays_best_speed(self, arr: List[int]) -> int:
+        n = len(arr)
+        s = 0
+        freq = 0
+        n = len(arr)
+        for i in range(n):
+            freq = freq - (i + 1) // 2 + (n - i + 1) // 2
+            s += freq*arr[i]
+        return s
+
+    def sumOddLengthSubarrays_best_memory(self, arr: List[int]) -> int:
+        ans=0
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)+1):
+                t=arr[i:j]
+                if len(t)%2!=0:
+                    ans+=sum(t)
+        return ans

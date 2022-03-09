c2536f03c04ff44299507614efe7eb41d16d1090
Evgeniy <ferno1@yandex.ru>
2022-03-09 22:54:41 +0300

    997

--- 900 - 999/997 - Find the Town Judge/997 - Find the Town Judge.py
+++ 900 - 999/997 - Find the Town Judge/997 - Find the Town Judge.py
@@ -6,6 +6,21 @@ class Solution:
             town[item[0]-1] -= 1
         return -1 if n-1 not in town else town.index(n-1)+1
 
+    def findJudge_study_plan(self, n: int, trust):  # 69.58% 91.83%
+        if not trust:
+            if n == 1:
+                return 1
+            else:
+                return -1
+        town = defaultdict(set)
+        for start, end in trust:
+            town[start].add(end)
+        out = set.intersection(*town.values()) - set(town)
+        if not out or len(town) < n-1:
+            return -1
+        out = out.pop()
+        return out if out not in town else -1
+
     def findJudge_best_speed(self, n: int, trust: List[List[int]]) -> int:
         people = [-1] + [0] * (n) 
         for source, target in trust:
@@ -17,3 +32,16 @@ class Solution:
             return judges.pop()
         else:
             return -1 
+
+    def findJudge_best_memory(self, n: int, trust: List[List[int]]) -> int:
+        if len(trust) < n - 1:
+            return -1
+        indegree = [0] * (n + 1)
+        outdegree = [0] * (n + 1)
+        for a, b in trust:
+            outdegree[a] += 1
+            indegree[b] += 1
+        for i in range(1, n + 1):
+            if indegree[i] == n - 1 and outdegree[i] == 0:
+                return i
+        return -1

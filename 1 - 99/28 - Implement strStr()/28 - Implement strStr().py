1fbbcc6a5fb2b1640a85f243e426bf118572a0f5
Evgeniy <ferno1@yandex.ru>
2022-03-11 12:46:41 +0300

    28

--- 1 - 99/28 - Implement strStr()/28 - Implement strStr().py
+++ 1 - 99/28 - Implement strStr()/28 - Implement strStr().py
@@ -1,7 +1,5 @@
 class Solution:
-
-    # 1
-    def strStr(self, haystack: str, needle: str) -> int:
+    def strStr(self, haystack: str, needle: str) -> int:  # 98.17% 25.15%
         if needle == '':
             return 0
         try:
@@ -9,15 +7,13 @@ class Solution:
         except:
             return -1
 
-    # 3
     def strStr_index(self, haystack, needle):
         if needle not in haystack:
             return -1
         else:
             return haystack.index(needle)
 
-    # 2
-    def strStr__two(self, haystack: str, needle: str) -> int:
+    def strStr_two(self, haystack: str, needle: str) -> int:  #  95.26% 32.82%
         if needle == '':
             return 0
         for i in range(len(haystack)-len(needle) + 1):
@@ -25,6 +21,17 @@ class Solution:
                 return i
         return -1
 
+    def strStr_study_plan(self, haystack, needle) -> int:  # 98.17% 42.88%
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
 
-x = Solution()
-print(x.strStr_index('a', 'a'))
+    def strStr_best_speed(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+    def strStr_best_memory(self, haystack: str, needle: str) -> int:
+        for i in range(0,len(haystack)-len(needle)+1):
+            if needle==haystack[i:len(needle)+i]:
+                return i
+        return -1

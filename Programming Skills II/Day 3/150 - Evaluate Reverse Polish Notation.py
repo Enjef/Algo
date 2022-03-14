075ba3a1d328bda818d7711acef3a5318503a4fd
Evgeniy <ferno1@yandex.ru>
2022-03-14 19:07:28 +0300

    Day 3

---
+++ Programming Skills II/Day 3/150 - Evaluate Reverse Polish Notation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token.strip('-+').isdigit():
+                stack.append(token)
+            else:
+                if len(stack) == 1:
+                    stack.append((-1 if token == '-' else 1)*int(stack.pop()))
+                    continue
+                second, first = int(stack.pop()), int(stack.pop())
+                if token == '+':
+                    stack.append(first+second)
+                elif token == '-':
+                    stack.append(first-second)
+                elif token == '*':
+                    stack.append(first*second)
+                else:
+                    stack.append(first/second)
+        return int(stack.pop())

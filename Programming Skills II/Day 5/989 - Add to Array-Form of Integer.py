b917187c006912f0e4733d4bd0dd95e7de451838
Evgeniy <ferno1@yandex.ru>
2022-03-15 20:41:13 +0300

    989

--- Programming Skills II/Day 5/989 - Add to Array-Form of Integer.py
+++ Programming Skills II/Day 5/989 - Add to Array-Form of Integer.py
@@ -1,6 +1,12 @@
 class Solution:
-    def addToArrayForm(self, num, k):  #
+    def addToArrayForm(self, num, k):  # 55.43% 37.80%
         cur = 0
         for el in num:
             cur = cur*10 + el
         return [int(x) for x in str(cur+k)]
+
+    def addToArrayForm_v2(self, num, k):  # 28.93% 94.47%
+        cur = 0
+        for el in num:
+            cur = cur*10 + el
+        return list(str(cur+k))

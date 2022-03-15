af70140db4a674a49dba6e0032fb938e676202d0
Evgeniy <ferno1@yandex.ru>
2022-03-15 20:28:05 +0300

    67

--- 1 - 99/67 - Add Binary/67 - Add Binary.py
+++ 1 - 99/67 - Add Binary/67 - Add Binary.py
@@ -33,3 +33,18 @@ class Solution:
 
     def addBinary_best(self, a: str, b: str) -> str:
         return bin(int(a, 2) + int(b, 2)).replace("0b", "")
+
+
+    def addBinary_best_speed(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
+        a = list(a)
+        b = list(b)
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            result += str(carry %2)
+            carry //= 2
+        return result[::-1]

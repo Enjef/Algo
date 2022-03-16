77823d5e93bc2d7568fd56416857173ebff07fb5
Evgeniy <ferno1@yandex.ru>
2022-03-16 14:24:29 +0300

    58

--- 1 - 99/58 - Length of Last Word/58 - Length of Last Word.py
+++ 1 - 99/58 - Length of Last Word/58 - Length of Last Word.py
@@ -1,5 +1,5 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
+    def lengthOfLastWord(self, s: str) -> int:  # 85.19% 54.59%
         out = 0
         if len(s) == 1 and s[0] == ' ':
             return 0
@@ -14,6 +14,16 @@ class Solution:
             return out
         return 0
 
+    def lengthOfLastWord_v3(self, s: str) -> int:  # 94.39% 87.16%
+        res = 0
+        i = len(s) - 1
+        while i > -1 and s[i] == ' ':
+            i -= 1
+        while i > -1 and s[i] != ' ':
+            res += 1
+            i -= 1
+        return res
 
-x = Solution()
-print(x.lengthOfLastWord('a '))
+    def lengthOfLastWord_v2(self, s: str) -> int:  # 85.19% 54.59%
+        return len(s.split()[-1])
+

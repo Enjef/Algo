332ec1695c338ec1b396e0483134e571343c63e9
Evgeniy <ferno1@yandex.ru>
2022-04-01 15:29:25 +0300

    1721

---
+++ 1700 - 1799/1721 - Swapping Nodes in a Linked List/1721 - Swapping Nodes in a Linked List.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head, k):  # 24.77% 53.21%
+        first = second = end = head
+        i = 0
+        while end.next:
+            if i < k-1:
+                first = first.next
+            if i > k-2:
+                second = second.next
+            end = end.next
+            i += 1
+        first.val, second.val = second.val, first.val
+        return head
+
+    def swapNodes_best_speed(self, head: ListNode, k: int) -> ListNode:
+        first = last = head
+        for i in range(1, k):
+            first = first.next
+        null_checker = first 
+        while null_checker.next:
+            last = last.next
+            null_checker = null_checker.next
+        first.val, last.val = last.val, first.val
+        return head

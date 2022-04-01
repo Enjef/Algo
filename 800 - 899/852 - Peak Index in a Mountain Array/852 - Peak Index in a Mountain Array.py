89e7dbf1b35c7bfbf029498824dbc447ef5555c7
Evgeniy <ferno1@yandex.ru>
2022-04-01 19:48:06 +0300

    852

--- 800 - 899/852 - Peak Index in a Mountain Array/852 - Peak Index in a Mountain Array.py
+++ 800 - 899/852 - Peak Index in a Mountain Array/852 - Peak Index in a Mountain Array.py
@@ -5,15 +5,39 @@ class Solution:
             arr[0] = 0
         return arr.index(max(arr))
 
-    def peakIndexInMountainArray_best(self, arr: List[int]) -> int:
-        l, r, peak = 0, len(arr)-1, 0
-        while l <= r:
-            m = (l + r) // 2
-            if arr[m-1] < arr[m] > arr[m+1]:
-                peak = m
-                break
-            elif arr[m-1] < arr[m] < arr[m+1]:
-                l = m+1
+    def peakIndexInMountainArray_v2(self, arr):  # 5.07% 61.14%
+        left, right = 0, len(arr)-1
+        while left < right:
+            mid = left + (right-left)//2
+            if arr[mid] < arr[mid+1]:
+                left = mid + 1
             else:
-                r = m-1
-        return peak
+                right = mid
+        return left
+
+    def peakIndexInMountainArray_best_speed(self, arr: List[int]) -> int:
+        beg = 1
+        end = len(arr) - 2
+        while beg <= end:
+            mid = (beg + end) // 2
+            if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid - 1] < arr[mid] < arr[mid + 1]:
+                beg = mid + 1
+            elif arr[mid - 1] > arr[mid] > arr[mid + 1]:
+                end = mid - 1
+            elif arr[mid] < arr[mid - 1] and arr[mid] < arr[mid + 1]:
+                return -1
+        return -1
+
+    def peakIndexInMountainArray_2nd_best_speed(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] < arr[m + 1]:
+                l = m + 1
+            elif l == m:
+                break
+            else:
+                r = m
+        return l

3744f2908187010d521e8142b4bde7757d1ea394
Evgeniy <ferno1@yandex.ru>
2022-04-02 06:02:13 +0300

    2138

---
+++ 2100 - 2199/2138 - Divide a String Into Groups of Size k/2138. Divide a String Into Groups of Size k.py
@@ -0,0 +1,20 @@
+class Solution:
+    def divideString(self, s, k, fill):  # 66.99% 78.93%
+        out = [s[i:i+k] for i in range(0, len(s), k)]
+        print(out)
+        if len(out[-1]) < k:
+            out[-1] = ''.join([out[-1], fill*(k-len(out[-1]))])
+        return out
+
+    def divideString_best_speed(self, s: str, k: int, fill: str) -> List[str]:
+        l = []
+        if len(s) % k != 0:
+            s += fill*(k-len(s) % k)
+        for i in range(0, len(s), k):
+            l.append(s[i:i+k])
+        return l
+
+    def divideString_memory_best(self, s: str, k: int, fill: str) -> List[str]:
+        res = [s[i:i+k] for i in range(0, len(s), k)]
+        res[-1] += fill*(k-len(res[-1]))
+        return res

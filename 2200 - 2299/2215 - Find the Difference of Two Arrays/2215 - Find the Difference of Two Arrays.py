b40b07ea7721c3676586110d168e1b49095334d7
Evgeniy <ferno1@yandex.ru>
2022-04-06 20:36:27 +0300

    2215

---
+++ 2200 - 2299/2215 - Find the Difference of Two Arrays/2215 - Find the Difference of Two Arrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findDifference(self, nums1, nums2):  # 99.22% 60.66%
+        first, second = set(nums1), set(nums2)
+        return list(first-second), list(second-first)
+
+    def findDifference_best_speed(self, nums1, nums2):
+        def create_dict(x):
+            dict = {}
+            for i in x:
+                if i not in dict:
+                    dict[i] = 1
+            return dict
+
+        nums1 = create_dict(nums1)
+        nums2 = create_dict(nums2)
+        a = [x for x in nums1 if x not in nums2]
+        b = [x for x in nums2 if x not in nums1]
+        return [a, b]
+
+    def findDifference_best_memory(self, num1, num2):
+        a=list(set(num1)-set(num2))
+        b=list(set(num2)-set(num1))
+        return [a,b]

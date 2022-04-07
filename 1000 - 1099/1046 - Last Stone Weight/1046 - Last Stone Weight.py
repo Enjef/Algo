9df4e908b50d62c03f00593fd3191f5b515225be
Evgeniy <ferno1@yandex.ru>
2022-04-07 08:44:37 +0300

    1046

--- 1000 - 1099/1046 - Last Stone Weight/1046 - Last Stone Weight.py
+++ 1000 - 1099/1046 - Last Stone Weight/1046 - Last Stone Weight.py
@@ -25,7 +25,24 @@ class Solution:
             return 0
         return stones[0]
 
-    def lastStoneWeight_best_memory(self, stones: List[int]) -> int:
+    def lastStoneWeight_heap(self, stones: List[int]) -> int:  # 78.86% 17.07%
+        out = []
+        heapify(out)
+        for stone in stones:
+            heappush(out, -stone)
+        while len(out) >1:
+            heappush(out, heappop(out)-heappop(out))
+        return -heappop(out)
+
+    def lastStoneWeight_best_speed(self, stones: List[int]) -> int:
+        h = [-elem for elem in stones]
+        heapq.heapify(h)
+        while len(h) > 1:
+            a, b = heapq.heappop(h), heapq.heappop(h)
+            heapq.heappush(h, -abs(a - b))
+        return abs(h[0])
+
+    def lastStoneWeight_best_memory_old(self, stones: List[int]) -> int:
         stones = sorted(stones)
         while len(stones) > 1:
             y = stones.pop(-1)
@@ -34,3 +51,18 @@ class Solution:
                 stones.append(y-x)
                 stones = sorted(stones)
         return stones[0] if len(stones) == 1 else 0
+
+    def lastStoneWeight_best_memory(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] *= -1
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
+            z = abs(x - y)
+            if z != 0:
+                heapq.heappush(stones, z * -1)
+        if len(stones) == 1:
+            return stones[0] * -1
+        else:
+            return 0

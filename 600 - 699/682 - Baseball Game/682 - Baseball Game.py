b279abf3e78c589749aba0ea6c11d546d6576a2f
Evgeniy <ferno1@yandex.ru>
2022-04-10 13:22:14 +0300

    682

--- 600 - 699/682 - Baseball Game/682 - Baseball Game.py
+++ 600 - 699/682 - Baseball Game/682 - Baseball Game.py
@@ -11,17 +11,36 @@ class Solution(object):
             if i == '+':
                 out.append(out[-1] + out[-2])
         return sum(out)
-    
-    def calPoints_else(self, ops: List[str]) -> int:
-        out = []
-        for i in ops:
-            if i == 'C':
-                out.pop()
-            elif i == 'D':
-                out.append(2 * out[-1])
-            elif i == '+':
-                out.append(out[-1] + out[-2])
-            else:            
-                out.append(int(i))
-        return sum(out)  
 
+    def calPoints_daily(self, ops: List[str]) -> int:  # 84.23% 74.69%
+        stack = []
+        for operation in ops:
+            if operation == '+':
+                stack.append(stack[-2]+stack[-1])
+            elif operation == 'D':
+                stack.append(stack[-1]*2)
+            elif operation == 'C':
+                stack.pop()
+            else:
+                stack.append(int(operation))
+        return sum(stack)
+
+    def calPoints_best_memory(self, ops: List[str]) -> int:
+        if not ops:
+            return 0
+        stack = []
+        for op in ops:
+            if op.isdigit() or (op[0] == '-' and op[1:].isdigit()):
+                if op[0] == '-':
+                    stack.append(-int(op[1:]))
+                else:
+                    stack.append(int(op))
+            elif op == 'C':
+                stack.pop()
+            elif op == 'D':
+                new_value = stack[-1] * 2
+                stack.append(new_value)
+            elif op == '+':
+                new_value = stack[-1] + stack[-2]
+                stack.append(new_value)
+        return sum(stack)

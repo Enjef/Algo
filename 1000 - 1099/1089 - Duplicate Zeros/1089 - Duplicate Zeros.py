4ea2a92a2f3fb2c14bde58362038ae09c32d5abd
Evgeniy <ferno1@yandex.ru>
2022-05-02 19:48:20 +0300

    1089

--- 1000 - 1099/1089 - Duplicate Zeros/1089 - Duplicate Zeros.py
+++ 1000 - 1099/1089 - Duplicate Zeros/1089 - Duplicate Zeros.py
@@ -23,9 +23,9 @@ class Solution:
         """
         i = 0
         n = len(arr)
-        while(i<n):
+        while(i < n):
             if arr[i] == 0:
-                arr.insert(i,0)
+                arr.insert(i, 0)
                 i += 1
                 arr.pop()
             i += 1

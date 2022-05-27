3124371647f930ddfe3433aeb93f313cf95c826b
Evgeniy <ferno1@yandex.ru>
2022-05-27 22:13:16 +0300

    697

---
+++ 600 - 699/697 - Degree of an Array/697 - Degree of an Array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:  # 19.36% 84.19%
+        n = len(nums)
+        counter = {}
+        for i, num in enumerate(nums):
+            if num not in counter:
+                counter[num] = [0, n, 0]
+            counter[num][0] += 1
+            counter[num][1] = min(i, counter[num][1])
+            counter[num][2] = max(i, counter[num][2])
+        good = max(counter.values())[0]
+        candidates = [val for val in counter.values() if val[0] == good]
+        result = n
+        for candidate in candidates:
+            result = min(result, candidate[2]-candidate[1]+1)
+        return result
+        
+    def findShortestSubArray_best_speed(self, nums: List[int]) -> int:
+        left, right, count = {}, {}, {}
+        for i, x in enumerate(nums):
+            if x not in left:
+                left[x] = i
+            right[x] = i
+            count[x] = count.get(x, 0) + 1
+        ans = len(nums)
+        degree = max(count.values())
+        for x in count:
+            if count[x] == degree:
+                ans = min(ans, right[x] - left[x] + 1)
+        return ans

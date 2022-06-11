10cfe73d8e56dfbef21a62d6bdf0af726ac66a22
Evgeniy <ferno1@yandex.ru>
2022-06-12 02:38:10 +0300

    2300

--- 2300 - 2399/2300 - Successful Pairs of Spells and Potions/2300 - Successful Pairs of Spells and Potions.py
+++ 2300 - 2399/2300 - Successful Pairs of Spells and Potions/2300 - Successful Pairs of Spells and Potions.py
@@ -12,8 +12,10 @@ class Solution:
         return out
 
     def successfulPairs_v2(self, spells: List[int], potions: List[int], success: int) -> List[int]:  # 28.57% 57.14% (57.14% 57.14%)
-        return (potions.sort(),
-        [len(potions)-bisect_left(potions, success/spell, 0, len(potions)-1) if (potions[bisect_left(potions, success/spell, 0, len(potions)-1)])*spell >= success else 0 for spell in spells])[1]
+        return (potions.sort(), [len(potions)-bisect_left(potions, success/spell, 0, len(potions)-1) if (potions[bisect_left(potions, success/spell, 0, len(potions)-1)])*spell >= success else 0 for spell in spells])[1]
+
+    def successfulPairs_v3(self, spells: List[int], potions: List[int], success: int) -> List[int]:  # 57.14% 57.14% (71.43% 57.14%)
+        return (potions.sort(), [len(potions)-bisect_left(potions, ceil(success/spell)) for spell in spells])[1]
 
     def successfulPairs_lee215(self, spells, potions, success):
         potions.sort()

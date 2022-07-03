b9c4ecf6815ea12a317131aab05362db2464fe20
Evgeniy <ferno1@yandex.ru>
2022-07-03 12:27:58 +0300

    Day 7 Binary Search

---
+++ Study Plan - LeetCode 75 - Level 1/Day 7 Binary Search/704 - Binary Search.py
@@ -0,0 +1,13 @@
+class Solution:
+    # 86.15% 71.95%
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            if nums[mid] < target:
+                left = mid + 1
+        return -1

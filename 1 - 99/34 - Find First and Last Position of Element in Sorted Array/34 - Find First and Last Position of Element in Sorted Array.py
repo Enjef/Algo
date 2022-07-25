0697402863396f4bceea94482b0eebb899923751
Evgeniy <ferno1@yandex.ru>
2022-07-25 12:19:52 +0300

    34 daily

--- 1 - 99/34 - Find First and Last Position of Element in Sorted Array/34 - Find First and Last Position of Element in Sorted Array.py
+++ 1 - 99/34 - Find First and Last Position of Element in Sorted Array/34 - Find First and Last Position of Element in Sorted Array.py
@@ -1,5 +1,6 @@
 class Solution:
-    def searchRange(self, nums, target):  # 46.18% 94.38%
+    # 46.18% 94.38%
+    def searchRange(self, nums, target):
         n = len(nums)-1
         left, right = 0, n
         while left < right:
@@ -20,9 +21,32 @@ class Solution:
                 left = mid + 1
         return first, right
 
+    # 31.35% 50.23%
+    def searchRange_daily(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)-1
+        left, right = 0, n
+        while left < right:
+            mid = (left+right)//2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        if not nums or nums[left] != target:
+            return -1, -1
+        first = left
+        right = n
+        while left < right:
+            mid = ceil((left+right)/2) # mid = (left+right+1)//2
+            if nums[mid] <= target:
+                left = mid
+            else:
+                right = mid - 1
+        return first, right
+
     def searchRange_best_speed(self, nums, target):
         if nums == None or len(nums) == 0:
             return [-1, -1]
+
         def find(arr, target):
             low, high = 0, len(arr)-1
             while low <= high:
@@ -34,10 +58,10 @@ class Solution:
                 else:
                     low = mid + 1
             return False
-        
+
         if not find(nums, target):
             return [-1, -1]
-        left  = bisect.bisect_left(nums, target)
+        left = bisect.bisect_left(nums, target)
         right = bisect.bisect_right(nums, target)
         return [left, right-1]
 
@@ -46,7 +70,7 @@ class Solution:
             return [-1, -1]
 
         def binary_search(arr, target):
-            left = 0 
+            left = 0
             right = len(arr) - 1
             while left < right:
                 mid = left + (right - left) // 2
@@ -55,8 +79,8 @@ class Solution:
                 else:
                     right = mid
             return left
-        
-        idx = binary_search(nums,target)
+
+        idx = binary_search(nums, target)
         if nums[idx] != target:
             return [-1, -1]
         right = left = idx

cfa0762ac95608e68776f98f28dde262c70543f4
Evgeniy <ferno1@yandex.ru>
2022-07-30 23:51:58 +0300

    916 + best

--- 900 - 999/916 - Word Subsets/916 - Word Subsets.py
+++ 900 - 999/916 - Word Subsets/916 - Word Subsets.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Solution:
     #  50.17% 6.51% (72.64% 7.49%)
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
@@ -15,3 +18,59 @@ class Solution:
             else:
                 out.append(i)
         return [words1[i] for i in out]
+
+    def wordSubsets_best_speed(self, words1: List[str], words2: List[str]) -> List[str]:
+        Bfreq, ans, cmax = {}, [], 0
+        for word in words2:
+            for char in word:
+                count = word.count(char)
+                if char in Bfreq:
+                    diff = count - Bfreq[char]
+                    if diff > 0:
+                        Bfreq[char] = count
+                        cmax += diff
+                else:
+                    Bfreq[char] = count
+                    cmax += count
+            if cmax > 10:
+                return ans
+        for word in words1:
+            if len(word) < cmax:
+                continue
+            for char in Bfreq:
+                if word.count(char) < Bfreq[char]:
+                    break
+            else:
+                ans.append(word)
+        return ans
+
+    def getFreqOfChar(self, string):
+        freq = [0] * 26
+        for j in string:
+            freq[ord(j)-ord('a')] += 1
+        return freq
+
+    def wordSubsets_best_memory(self, words1: List[str], words2: List[str]) -> List[str]:
+        result = []
+        maxCount = [0] * 26
+        for word in words2:
+            charFreq = self.getFreqOfChar(word)
+            for j in range(26):
+                maxCount[j] = max(maxCount[j], charFreq[j])
+
+        for idx, word in enumerate(words1):
+            word1CharFreq = self.getFreqOfChar(word)
+            flag = True
+            for i in range(26):
+                if maxCount[i] > word1CharFreq[i]:
+                    flag = False
+                    break
+            if flag:
+                result.append(word)
+        return result
+
+    def wordSubsets_2nd_best_memory(self, words1: List[str], words2: List[str]) -> List[str]:
+        count = collections.Counter()
+        for b in words2:
+            count |= collections.Counter(b)
+        return [a for a in words1 if not count - Counter(a)]

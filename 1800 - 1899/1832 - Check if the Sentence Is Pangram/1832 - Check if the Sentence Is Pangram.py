ff721ce2316e77d6d48ad74057e4b3aa52a5267e
Evgeniy <ferno1@yandex.ru>
2022-10-17 09:34:24 +0300

    1832

--- 1800 - 1899/1832 - Check if the Sentence Is Pangram/1832 - Check if the Sentence Is Pangram.py
+++ 1800 - 1899/1832 - Check if the Sentence Is Pangram/1832 - Check if the Sentence Is Pangram.py
@@ -1,8 +1,36 @@
 class Solution(object):
-    def checkIfPangram(self, sentence):  # 36%
+    # 36%
+    def checkIfPangram(self, sentence):
         abc_set = set('abcdefghijklmnopqrstuvwxyz')
         sentence = set(sentence)
         return abc_set == sentence
 
-    def top_four(self, sentence):
+    # 73.44% 11.66%
+    def checkIfPangram_daily(self, sentence: str) -> bool:
+        return set(sentence) == set(ascii_lowercase)
+
+    def checkIfPangram_best_speed(self, sentence: str) -> bool:
+        l = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        ]
+        for i in range(len(l)):
+            if l[i] not in sentence:
+                return 0
+        else:
+            return 1
+
+    def checkIfPangram_2nd_best_speed(self, sentence: str) -> bool:
+        s = set()
+        for c in sentence:
+            s.add(c)
+        return len(s) == 26
+
+    def checkIfPangram_best_memory(self, sentence: str) -> bool:
+        counter = 0
+        for c in sentence:
+            counter |= (1 << (ord(c) - ord('a')))
+        return counter == (1 << 26) - 1
+
+    def checkIfPangram_2d_best_memory(self, sentence: str) -> bool:
         return len(set(sentence)) == 26

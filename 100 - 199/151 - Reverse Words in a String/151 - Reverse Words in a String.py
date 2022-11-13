d2f108288f6d5b75349a7aebddc9577909911289
Evgeniy <ferno1@yandex.ru>
2022-11-13 12:39:43 +0300

    151

--- 100 - 199/151 - Reverse Words in a String/151 - Reverse Words in a String.py
+++ 100 - 199/151 - Reverse Words in a String/151 - Reverse Words in a String.py
@@ -3,3 +3,35 @@ class Solution:
         s = [x for x in s.split() if x != ' ']
         s.reverse()
         return ' '.join(s)
+
+    # 83.70% 16.15%
+    def reverseWords_v2(self, s: str) -> str:
+        return ' '.join(reversed([x for x in s.split() if x]))
+
+
+class Solution_best_split:
+    def reverseWords_1st(self, s: str) -> str:
+        return ' '.join(reversed(s.split()))
+
+    def reverseWords_2nd(self, s: str) -> str:
+        return ' '.join(s.split()[::-1])
+
+
+class Solution_best_memory:
+    def reverseWords(self, s: str) -> str:
+        ans = ''
+        i = 0
+        word = ''
+        while i < len(s):
+            if s[i] != ' ':
+                word += s[i]
+            elif word != '':
+                ans = word + ' ' + ans
+                word = ''
+            i += 1
+        if word != '':
+            ans = word + ' ' + ans
+        return ans.strip()
+
+    def reverseWords_2nd(self, s: str) -> str:
+        return ' '.join(s.split()[::-1])
